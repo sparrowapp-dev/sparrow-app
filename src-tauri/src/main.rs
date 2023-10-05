266411cf9f39aad0c41afdf16e4a98d3c11ddcb7
astitva-techdome <astitva.patle@techdome.net.in>
2023-10-05 19:52:05 +0530

    feat: Add REST api handler in rust[SP-108]

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,11 +7,12 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 use std::error::Error;
-
+use serde_json::Value;
+use std::collections::HashMap;
 #[tauri::command]
-fn make_http_request_command(url: String) -> String {
+fn make_http_request_command(url: String, headers: HashMap<String, String>) -> String {
     // You can call an async function from here
-    let result = make_http_request(url);
+    let result = make_http_request(url, headers);
     let result_string = match result {
         Ok(value) => value,
         Err(err) => format!("Error: {:?}", err), // Handle the error case here
@@ -21,8 +22,13 @@ fn make_http_request_command(url: String) -> String {
 }
 
 #[tokio::main]
-async fn make_http_request(url: String) -> Result<String, Box<dyn Error>> {
-    let resp = reqwest::get(url)
+async fn make_http_request(url: String, headers: HashMap<String, String>) -> Result<String, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let mut request = client.get(url);
+    for (key, value) in headers.iter() {
+        request = request.header(key, value);
+    }
+    let resp = request.send()
     .await?
     .text()
     .await?;
@@ -30,9 +36,100 @@ println!("{:#?}", resp);
 Ok(resp)
 }
 
+
+#[tauri::command]
+fn make_post_http_request_command(url: String, body: serde_json::Value, headers: HashMap<String, String>) -> String {
+    // You can call an async function from here
+    let result = make_post_http_request(url, body, headers);
+    let result_string = match result {
+        Ok(value) => value,
+        Err(err) => format!("Error: {:?}", err), // Handle the error case here
+    };
+    println!("Result from async function: {:?}", result_string);
+    return result_string;
+}
+
+#[tokio::main]
+async fn make_post_http_request(url: String, body: serde_json::Value, headers: HashMap<String, String>) -> Result<String, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let mut request = client.post(url);
+    for (key, value) in headers.iter() {
+        request = request.header(key, value);
+    }
+    let resp = request.json(&body)
+        .send()
+        .await?
+        .text()
+        .await?;
+println!("{:#?}", resp);
+Ok(resp)
+}
+
+#[tauri::command]
+fn make_put_http_request_command(url: String, body: serde_json::Value, headers: HashMap<String, String>) -> String {
+    let result = make_put_http_request(url, body, headers);
+    let result_string = match result {
+        Ok(value) => value,
+        Err(err) => format!("Error: {:?}", err),
+    };
+    println!("Result from async function: {:?}", result_string);
+    return result_string;
+}
+
+#[tokio::main]
+async fn make_put_http_request(url: String, body: serde_json::Value, headers: HashMap<String, String>) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    
+    let mut request = client.put(url);
+    for (key, value) in headers.iter() {
+        request = request.header(key, value);
+    }
+    let resp = request.json(&body)
+        .send()
+        .await?
+        .text()
+        .await?;
+    
+    println!("{:#?}", resp);
+    Ok(resp)
+}
+
+
+#[tauri::command]
+fn make_delete_http_request_command(url: String, headers: HashMap<String, String>) -> String {
+    let result = make_delete_http_request(url, headers);
+    let result_string = match result {
+        Ok(value) => value,
+        Err(err) => format!("Error: {:?}", err),
+    };
+    println!("Result from async function: {:?}", result_string);
+    return result_string;
+}
+
+#[tokio::main]
+async fn make_delete_http_request(url: String, headers: HashMap<String, String>) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let mut request = client.delete(url);
+
+    for (key, value) in headers.iter() {
+        request = request.header(key, value);
+    }
+    
+    let resp = request.send().await?.text().await?;
+    
+    println!("{:#?}", resp);
+    Ok(resp)
+
+    
+}
+
+
+
+
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet,make_http_request_command])
+        .invoke_handler(tauri::generate_handler![greet,make_http_request_command, make_post_http_request_command, make_put_http_request_command, make_delete_http_request_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
