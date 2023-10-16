aed4627d5f0b34609e71efd2f4d7f40d9d00d017
Md Asif Raza <asif.raza@techdome.net.in>
2023-10-16 17:58:50 +0530

    feat: Add code editor to request body [SP-84]

--- src/pages/dashboard/collections/request-response-section/request-body/RequestBody.svelte
+++ src/pages/dashboard/collections/request-response-section/request-body/RequestBody.svelte
@@ -1,7 +1,41 @@
 <script>
+  import { JSONEditor } from "svelte-jsoneditor";
 
+  let content = {
+    text: `
+      {
+        "nameS": "Testing",
+        "email": "testing@testing.com"
+      }`, // can be used to pass a stringified JSON document instead
+    json: {
+      name: "Testin",
+      email: "testing@testing.com",
+    },
+  };
+
+  $: console.log("contents changed:", content);
 </script>
 
-<div>req</div>
+<div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown button
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </div>
+</div>
+<div class="editor ps-3 rounded">
+  <JSONEditor bind:content />
+</div>
 
-<style></style>
+<!-- <style>
+    .editor {
+      width: 100%;
+      max-width: 600px;
+      height: 400px;
+    }
+  </style> -->
