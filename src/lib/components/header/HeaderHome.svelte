4dc4853c46842b74ecda0daa232c3cea117481bc
Md Asif Raza <asif.raza@techdome.net.in>
2023-10-20 12:07:26 +0530

    fix: Update logout UI [SP-44]

--- src/lib/components/header/HeaderHome.svelte
+++ src/lib/components/header/HeaderHome.svelte
@@ -15,6 +15,12 @@
   import { useNavigate } from "svelte-navigator";
   import { notifications } from "$lib/utils/notifications";
   import { setUser } from "$lib/store/auth.store";
+  import signout from "$lib/assets/signout.svg";
+  import shortcut from "$lib/assets/shortcut.svg";
+  import about from "$lib/assets/about.svg";
+  import settings from "$lib/assets/settings.svg";
+  import account from "$lib/assets/account.svg";
+
   const navigate = useNavigate();
   const onMinimize = () => {
     appWindow.minimize();
@@ -96,21 +102,36 @@
         </button>
       </div>
       <div class="col-4">
-        <button class="btn btn-black position-relative" >
-          <img src={profileIcon} on:click={()=>{setTimeout(() => {
-            profile = true;
-          }, 100);}} alt="" />
-          <div class="position-absolute bg-white" style="display: {profile ? 'block' : 'none'}; top: 30px; right: 0; width: 219px;"  on:click={()=>{profile = false;}}>
-            <p on:click={()=>{
-              // logout();
-              profile = false;
-              }}></p>
-            <p on:click={()=>{
-              // logout();
-              profile = false;
-              }}>Sign Out</p>
+        <div class="position-relative">
+          <button class="btn btn-black" style="border:none; outline:none;" >
+            <img src={profileIcon} on:click={()=>{setTimeout(() => {
+              profile = true;
+            }, 100);}} alt=""/>
+          </button>
+          <div class="rounded profile-explorer position-absolute text-color-white py-1" style="border: 1px solid #313233; background-color: rgba(0,0,0,0.7); backdrop-filter: blur(10px); display: {profile ? 'block' : 'none'}; top: 40px; right: 0; width: 219px;"  on:click={()=>{profile = false;}}>
+              <div class="cursor-pointer d-flex align-items-center flex-start px-3 height: 26px" on:click={()=>{
+                profile = false;
+                }}><img src={account} alt=""><span class="m-2" style="font-size: 12px;">View Account</span>
+              </div>
+              <div class="cursor-pointer d-flex align-items-center flex-start px-3 height: 26px" on:click={()=>{
+                profile = false;
+                }}><img src={settings} alt=""><span class="m-2" style="font-size: 12px;">Notification Settings</span>
+              </div>
+              <div class="cursor-pointer d-flex align-items-center flex-start px-3 height: 26px" on:click={()=>{
+                profile = false;
+                }}><img src={shortcut} alt=""><span class="m-2" style="font-size: 12px;">App Shortcuts</span>
+              </div>
+              <div class="cursor-pointer d-flex align-items-center flex-start px-3 height: 26px" on:click={()=>{
+                profile = false;
+                }}><img src={about} alt=""><span class="m-2" style="font-size: 12px;">About Sparrow</span>
+              </div>
+              <div class="cursor-pointer d-flex align-items-center flex-start px-3 height: 26px" on:click={()=>{
+                logout();
+                }}><img src={signout} alt=""><span class="m-2" style="font-size: 12px;">Sign Out</span>
+              </div>
           </div>
-        </button>
+
+        </div>
       </div>
     </div>
 
@@ -147,7 +168,8 @@
     background-clip: text;
     color: transparent;
   }
-  .profile-tab-active{
-    display: block !important;
+  .cursor-pointer{
+    cursor: pointer;
   }
+
 </style>
