b7de69bd94a537542217a4f530b1be2cac01584c
Md Asif Raza <asif.raza@techdome.net.in>
2023-12-08 17:39:05 +0530

    fix: throw rigister to login component [SP-189]

--- src/pages/Auth/register-page/register-page.ts
+++ src/pages/Auth/register-page/register-page.ts
@@ -1,8 +1,4 @@
-import { resizeWindowOnLogin } from "$lib/components/header/window-resize";
 import { registerUser } from "$lib/services/auth.service";
-import { setUser } from "$lib/store/auth.store";
-import constants from "$lib/utils/constants";
-import { jwtDecode, setAuthJwt } from "$lib/utils/jwt";
 import { notifications } from "$lib/utils/notifications";
 import { checkValidation, registrationSchema } from "$lib/utils/validation";
 import { navigate } from "svelte-navigator";
@@ -11,12 +7,8 @@ const handleRegister = async (userData) => {
   const response = await registerUser(userData);
 
   if (response.isSuccessful) {
-    setAuthJwt(constants.AUTH_TOKEN, response?.data?.data?.accessToken.token);
-    setAuthJwt(constants.REF_TOKEN, response?.data?.data?.refreshToken.token);
-    setUser(jwtDecode(response.data?.data?.accessToken?.token));
     notifications.success("Registration successful!");
     navigate("/login");
-    await resizeWindowOnLogin();
   } else {
     notifications.error(response.message);
     throw "error registering user: " + response.message;
