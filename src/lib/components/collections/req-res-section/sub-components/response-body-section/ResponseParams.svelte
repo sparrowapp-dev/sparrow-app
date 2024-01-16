b5a83cd36954d4611db0d1ee84a7fdab093d69c4
Md Asif Raza <itsmdasifraza@gmail.com>
2024-01-16 12:23:00 +0530

    style: response status code [SP-365]

--- src/lib/components/collections/req-res-section/sub-components/response-body-section/ResponseParams.svelte
+++ src/lib/components/collections/req-res-section/sub-components/response-body-section/ResponseParams.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-  import {
-    RequestProperty,
-    ResponseSection,
-  } from "$lib/utils/enums/request.enum";
+  import { ResponseSection } from "$lib/utils/enums/request.enum";
   import type { CollectionsMethods } from "$lib/utils/interfaces/collections.interface";
-  import { generateSampleRequest } from "$lib/utils/sample/request.sample";
   import ResponseBody from "./ResponseBody.svelte";
-  import { v4 as uuidv4 } from "uuid";
 
   import ResponseHeader from "./ResponseHeader.svelte";
-  import { UntrackedItems } from "$lib/utils/enums/item-type.enum";
-  import { notifications } from "$lib/utils/notifications";
 
   export let response;
   export let apiState;
@@ -74,53 +67,20 @@
         </span>
       </span>
     </div>
-
-    <div class="d-flex gap-2">
-      <button
-        class="cursor-pointer ps-1 pe-1 border-0 rounded d-flex align-items-center justify-content-center text-backgroundColor gap-1 status-primary1"
-        style="font-size: 12px; font-weight:600"
-      >
-        <span>{statusCode}</span>
-      </button>
-      <button
-        class="d-flex align-items-center ps-1 pe-1 border-0 justify-content-center rounded text-backgroundColor gap-1 time-primary1"
-        style="font-size: 12px;font-weight:600"
-      >
-        <span>{timeTaken}</span>
-        <p class="mb-0">ms</p>
-      </button>
-      <button
-        class="d-flex align-items-center ps-1 pe-1 justify-content-center rounded border-0 text-backgroundColor gap-1 size-primary1"
-        style="font-size: 12px;font-weight:600"
-      >
-        <span>{sizeinKb?.toFixed(2)}</span>
-        <p class="mb-0">KB</p>
-      </button>
-      <button
-        class="clear-button d-flex align-items-center justify-content-center rounded border-0 gap-1"
-        style="font-size: 12px;font-weight:600"
-        on:click={() => {
-          const response = generateSampleRequest(
-            UntrackedItems.UNTRACKED + uuidv4(),
-            new Date().toString(),
-          ).property.request.response;
-          collectionsMethods.updateRequestProperty(
-            response,
-            RequestProperty.RESPONSE,
-            currentTabId,
-          );
-          notifications.success("Response Cleared");
-        }}
-      >
-        Clear
-      </button>
-    </div>
   </div>
 </div>
 
 <div class="d-flex align-items-center justify-content-center w-100">
   {#if apiState.responseSection === ResponseSection.RESPONSE}
-    <ResponseBody {apiState} {collectionsMethods} {response} {currentTabId} />
+    <ResponseBody
+      {apiState}
+      {collectionsMethods}
+      {response}
+      {currentTabId}
+      {sizeinKb}
+      {timeTaken}
+      {statusCode}
+    />
   {:else if apiState.responseSection === ResponseSection.HEADERS}
     <ResponseHeader {responseHeader} />
   {/if}
@@ -136,30 +96,6 @@
     border-bottom: 3px solid var(--send-button);
   }
 
-  .status-primary1 {
-    background: var(--success-color);
-    padding-top: 1px;
-    padding-bottom: 1px;
-  }
-
-  .clear-button {
-    background: var(--border-color);
-    color: var(--workspace-hover-color);
-    font-family: Roboto;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 18px;
-    letter-spacing: 0em;
-    text-align: center;
-    padding: 4px 8px 4px 8px;
-  }
-
-  .time-primary1 {
-    background: var(--button-color);
-  }
-  .size-primary1 {
-    background: var(--button-color);
-  }
   .cursor-pointer {
     cursor: pointer;
   }
