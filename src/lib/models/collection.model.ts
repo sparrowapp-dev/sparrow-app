4c327f150aafe21c43e58de5a7a692eb9ea17359
Nayan Lakhwani <nayanlakhwani@Nayans-MacBook-Pro.local>
2024-03-03 07:57:20 -0500

    feat: add auth info while saving api request[]

--- src/lib/models/collection.model.ts
+++ src/lib/models/collection.model.ts
@@ -46,7 +46,14 @@ const requestItems = {
   selectedRequestBodyType: {
     type: "string",
   },
+  selectedRequestAuthType: {
+    type: "string",
+  },
   queryParams: {
+    type: "array",
+    properties: params,
+  },
+  auth: {
     type: "array",
     properties: params,
   },
@@ -115,7 +122,7 @@ export const collectionSchemaLiteral = {
     separator: "|",
   },
   type: "object",
-  version: 1,
+  version: 2,
   properties: {
     collectionId: {
       type: "string",
