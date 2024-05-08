b964300b367d342b4de714ba268d2522f22a58aa
Md Asif Raza <itsmdasifraza@gmail.com>
2024-05-08 11:38:06 +0530

    style: rest explorer [#737]
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/packages/@workspaces/features/rest-explorer/layout/RestExplorer.svelte
+++ src/packages/@workspaces/features/rest-explorer/layout/RestExplorer.svelte
@@ -245,7 +245,12 @@
             class="position-relative "
           >
             <!-- Request Pane -->
-            <div class="h-100 position-relative pb-3">
+            <div
+              class="h-100 position-relative {$requestSplitterDirection ===
+              'horizontal'
+                ? 'pb-3'
+                : 'pe-3'}"
+            >
               <RequestNavigator
                 requestStateSection={$tab.property.request?.state
                   ?.requestNavigation}
@@ -306,7 +311,12 @@
             class="position-relative"
           >
             <!-- Response Pane -->
-            <div class="d-flex flex-column h-100 pt-3">
+            <div
+              class="d-flex flex-column h-100 {$requestSplitterDirection ===
+              'horizontal'
+                ? 'pt-3'
+                : 'ps-3'}"
+            >
               {#if $tab.property.request?.state?.isSendRequestInProgress}
                 <ResponseDefaultScreen />
                 <div
