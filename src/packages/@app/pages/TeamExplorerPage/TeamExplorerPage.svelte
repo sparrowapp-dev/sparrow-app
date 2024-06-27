92992fc9cfac7df9db47b272a4ebc2b2e1e87d49
Astitva Patle <ankurpatle18@gmail.com>
2024-06-27 10:55:21 +0530

    feat: establish team flow []

--- src/packages/@app/pages/TeamExplorerPage/TeamExplorerPage.svelte
+++ src/packages/@app/pages/TeamExplorerPage/TeamExplorerPage.svelte
@@ -1,1 +1,11 @@
-<div>Team explorer</div>
+<script lang="ts">
+  import type { Observable } from "rxjs";
+  import { TeamExplorer } from "@teams/features";
+  import { TeamExplorerPageViewModel } from "./TeamExplorerPage.ViewModel";
+  //   import type { Observable } from "rxjs";
+  import type { TeamDocument } from "@app/database/database";
+  const _viewModel = new TeamExplorerPageViewModel();
+  const activeTeam: Observable<TeamDocument> = _viewModel.openTeam;
+</script>
+
+<TeamExplorer openTeam={$activeTeam} />
