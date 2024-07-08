97affdd24945d8d25f8e0dc99085f66c3ca1bfb0
Md Asif Raza <itsmdasifraza@gmail.com>
2024-07-08 18:01:33 +0530

    style: add new tabs to settings [#1147]
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/packages/@teams/features/team-settings/components/settings-navigator/SettingsNavigator.svelte
+++ src/packages/@teams/features/team-settings/components/settings-navigator/SettingsNavigator.svelte
@@ -1,6 +1,17 @@
 <div class="settings-list w-30 pe-2">
-  <button class="settings-tab p-2 w-100 text-fs-12 rounded border-0">
+  <button
+    class="settings-tab settings-tab-active mb-1 p-2 w-100 text-fs-12 rounded border-0"
+  >
     Team Profile
+  </button>
+  <button class="settings-tab mb-1 p-2 w-100 text-fs-12 rounded border-0">
+    Authenthication
+  </button>
+  <button class="settings-tab mb-1 p-2 w-100 text-fs-12 rounded border-0">
+    Identity Provider
+  </button>
+  <button class="settings-tab mb-1 p-2 w-100 text-fs-12 rounded border-0">
+    Plugins
   </button>
 </div>
 
@@ -11,6 +22,9 @@
   }
   .settings-tab {
     text-align: left;
+    background-color: transparent;
+  }
+  .settings-tab-active {
     background-color: var(--bg-tertiary-250);
   }
 </style>
