29c7d1f295c16aadeff2966b03906838a7b53b51
Isha Dubey <ishadubey36@gmail.com>
2024-07-10 13:57:57 +0530

    fix: transfer ownership userstory completed [#1120]

--- src/packages/@library/ui/icon-fallback/IconFallback.svelte
+++ src/packages/@library/ui/icon-fallback/IconFallback.svelte
@@ -3,6 +3,7 @@ export let role;
 export let width = "36px";
 export let height = "36px";
 export let borderColor = "var(--border-color)";
+export let backgroundColor = "transparent";
 </script>
 
 <div
@@ -10,7 +11,8 @@ class="d-flex align-items-center justify-content-center"
 style="width: {width};
 border: 1px solid {borderColor} ;
 height: {height};
-border-radius: 50%;"
+border-radius: 50%;
+background-color:{backgroundColor};"
 >
 <span>{role.toUpperCase()}</span>
 </div>
