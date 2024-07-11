8a0e086fa9f91122e6eff7e0b924a137e9348453
Md Asif Raza <itsmdasifraza@gmail.com>
2024-07-11 19:13:39 +0530

    fix: add jsdoc comments [#1233]
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/packages/@library/ui/icon-fallback/IconFallback.svelte
+++ src/packages/@library/ui/icon-fallback/IconFallback.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
+  /**
+   * The character to be displayed.
+   */
   export let character = "";
+
+  /**
+   * The width of the element.
+   */
   export let width = "36px";
+
+  /**
+   * The height of the element.
+   */
   export let height = "36px";
+
+  /**
+   * The border color of the element.
+   */
   export let borderColor = "var(--border-secondary-400)";
+
+  /**
+   * The background color of the element.
+   */
   export let backgroundColor = "transparent";
+
+  /**
+   * The text color of the element.
+   */
   export let textColor = "var(--text-secondary-100)";
+
+  /**
+   * The font size of the text in the element.
+   */
   export let fontSize = "14px";
 </script>
 
