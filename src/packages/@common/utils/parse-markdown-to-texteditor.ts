3286342ddb23d411582c7f5963f9be68f1cd5d3c
Isha Dubey <isha.dubey@techdome.net.in>
2024-08-26 12:08:57 +0530

    fix: add changes[#1060]

--- src/packages/@common/utils/parse-markdown-to-texteditor.ts
+++ src/packages/@common/utils/parse-markdown-to-texteditor.ts
@@ -1,14 +1,4 @@
-interface Block {
-  id: string;
-  type: string;
-  data: {
-    text?: string;
-    style?: string;
-    items?: string[];
-    code?: string;
-  };
-  level?: number;
-}
+import type { Block } from "@common/types/editorjs";
 
 class MarkdownFormatter {
   /**
