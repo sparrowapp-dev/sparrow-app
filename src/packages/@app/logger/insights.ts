65172222755c6239702f5d3e9a7a57304828c19a
Astitva Patle <ankurpatle18@gmail.com>
2024-08-27 19:50:29 +0530

    fix: added env details []

--- src/packages/@app/logger/insights.ts
+++ src/packages/@app/logger/insights.ts
@@ -1,9 +1,9 @@
+import constants from "$lib/utils/constants";
 import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 
 export const appInsights = new ApplicationInsights({
   config: {
-    connectionString:
-      "InstrumentationKey=a1a41bff-dd46-494c-8da5-16deca5daf32;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/;ApplicationId=79949c18-deba-4d2e-8ab5-00c1f2496b56",
+    connectionString: `${constants.AZURE_INSIGHTS_CONNECTION_STRING}`,
     disableAjaxTracking: true,
     disableFetchTracking: true,
   },
