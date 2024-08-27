b6f0880c13b4f7d40d6eddebc6f959a5fd7fc14b
Astitva Patle <ankurpatle18@gmail.com>
2024-08-27 19:42:47 +0530

    feat: added logger for performance in api calling and rpc []

--- src/packages/@app/logger/insights.ts
+++ src/packages/@app/logger/insights.ts
@@ -4,10 +4,8 @@ export const appInsights = new ApplicationInsights({
   config: {
     connectionString:
       "InstrumentationKey=a1a41bff-dd46-494c-8da5-16deca5daf32;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/;ApplicationId=79949c18-deba-4d2e-8ab5-00c1f2496b56",
-    /* ...Other Configuration Options... */
-    enableAutoRouteTracking: false, // Automatically track route changes
-    loggingLevelConsole: 0,
-    loggingLevelTelemetry: 0,
+    disableAjaxTracking: true,
+    disableFetchTracking: true,
   },
 });
 appInsights.loadAppInsights();
