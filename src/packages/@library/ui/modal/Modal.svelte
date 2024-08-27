d99f42be7b210db61bfb093b738c5d2d037f6006
mitesh darda <mitesh.darda.0@gmail.com>
2024-08-27 16:51:34 +0530

    fix: fixed imports

--- src/packages/@library/ui/modal/Modal.svelte
+++ src/packages/@library/ui/modal/Modal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import CrossIcon from "@library/icons/Cross.svelte";
+  import { CrossIcon } from "@library/icons";
 
   import { scale, fade } from "svelte/transition";
   import warningIcon from "$lib/assets/download-warning.svg";
