a6b5f4670cfd3ac04e98caa8698d79ed565d2baf
Md Asif Raza <itsmdasifraza@gmail.com>
2024-09-11 16:57:11 +0530

    fix: environemnt permissions for viewer [#1509]

--- src/packages/@workspaces/features/environment-list/layout/EnvironmentList.svelte
+++ src/packages/@workspaces/features/environment-list/layout/EnvironmentList.svelte
@@ -138,11 +138,11 @@
 </script>
 
 <div
-  class={`d-flex flex-column  h-100 env-sidebar mt-1 bg-secondary-900   pt-0`}
+  class={`d-flex flex-column  h-100 env-sidebar mt-1 bg-secondary-900   pt-0 px-1`}
   style="font-weight: 500;"
 >
   <div
-    class="d-flex align-items-center p-2 rounded-1 me-0 mb-0"
+    class="d-flex align-items-center rounded-1 me-0 mb-0 pe-2"
     style="cursor:pointer; justify-content: space-between; height:32px;
       background-color: {isHovered
       ? 'var(--dropdown-option-hover)'
@@ -151,7 +151,10 @@
     on:mouseout={handleMouseOut}
     on:click={toggleExpandEnvironment}
   >
-    <div class="d-flex align-items-center ps-2">
+    <div
+      class="d-flex align-items-center ps-3 p-2"
+      style="width: calc(100% - 30px);"
+    >
       <img
         src={angleRight}
         class="me-3"
@@ -171,33 +174,36 @@
         Environment
       </p>
     </div>
-
-    <Tooltip
-      title={"Add Environment"}
-      placement={"bottom"}
-      distance={13}
-      show={isHovered}
-      zIndex={701}
-    >
-      <button
-        style="height: 24px; width:24px;"
-        class="add-icon-container border-0 rounded-1 d-flex justify-content-center align-items-center {isHovered
-          ? 'environment-active'
-          : 'environment-inactive'}"
-        on:click|stopPropagation={handleCreateEnvironment}
+    {#if loggedUserRoleInWorkspace !== WorkspaceRole.WORKSPACE_VIEWER}
+      <Tooltip
+        title={"Add Environment"}
+        placement={"bottom"}
+        distance={13}
+        show={isHovered}
+        zIndex={701}
       >
-        <PlusIcon
-          height={"18px"}
-          width={"18px"}
-          color={"var( --white-color)"}
-        />
-      </button>
-    </Tooltip>
+        <button
+          style="height: 24px; width:24px;"
+          disabled={loggedUserRoleInWorkspace ===
+            WorkspaceRole.WORKSPACE_VIEWER}
+          class="add-icon-container border-0 rounded-1 d-flex justify-content-center align-items-center {isHovered
+            ? 'environment-active'
+            : 'environment-inactive'}"
+          on:click|stopPropagation={handleCreateEnvironment}
+        >
+          <PlusIcon
+            height={"18px"}
+            width={"18px"}
+            color={"var( --white-color)"}
+          />
+        </button>
+      </Tooltip>
+    {/if}
   </div>
 
   {#if isExpandEnvironment}
-    <div class="overflow-auto h-100 mb-2" bind:this={scrollDiv}>
-      {#if filteredLocalEnvironment && localEnvironment.length === 0 && loggedUserRoleInWorkspace !== WorkspaceRole.WORKSPACE_VIEWER}
+    <div class="overflow-auto h-100 mb-2 ps-2" bind:this={scrollDiv}>
+      {#if filteredLocalEnvironment && localEnvironment.length === 0}
         {#if filteredGlobalEnvironment && filteredGlobalEnvironment.length > 0}
           <div class="mb-0">
             <p
@@ -227,36 +233,40 @@
             </p>
           </div>
         {/if}
-        <div class={`pb-2`}>
-          <p
-            class={`mx-1 add-env-desc-text mb-3 text-fs-12 mb-0 fw-normal text-center`}
-            style="color: var(--text-secondary-50)"
-          >
-            Add Environments in your Workspace for precise API testing with
-            relevant resources and constraints.
-          </p>
-          <p
-            class="mx-2 add-environment d-flex justify-content-center align-items-center border-radius-2"
-            style="color: var(--text-secondary-100);"
-            role="button"
-            on:click={() => {
-              onCreateEnvironment(localEnvironment);
-            }}
-          >
-            <PlusIcon
-              height={"22px"}
-              width={"22px"}
-              color={"var(--text-secondary-200)"}
-            />
-            <span
-              style="color: var(--text-secondary-200)"
-              class="ps-2 fw-bold text-fs-12">Add Environment</span
+        <div class={`pb-2 px-1`}>
+          {#if loggedUserRoleInWorkspace !== WorkspaceRole.WORKSPACE_VIEWER}
+            <p
+              class={`add-env-desc-text mb-3 text-fs-12 mb-0 fw-normal text-center`}
+              style="color: var(--text-secondary-50)"
             >
-          </p>
+              Add Environments in your Workspace for precise API testing with
+              relevant resources and constraints.
+            </p>
+            <button
+              disabled={loggedUserRoleInWorkspace ===
+                WorkspaceRole.WORKSPACE_VIEWER}
+              class="bg-transparent w-100 add-environment d-flex justify-content-center align-items-center border-radius-2"
+              style="color: var(--text-secondary-100);"
+              role="button"
+              on:click={() => {
+                onCreateEnvironment(localEnvironment);
+              }}
+            >
+              <PlusIcon
+                height={"22px"}
+                width={"22px"}
+                color={"var(--text-secondary-200)"}
+              />
+              <span
+                style="color: var(--text-secondary-200)"
+                class="ps-2 fw-bold text-fs-12">Add Environment</span
+              >
+            </button>
+          {/if}
         </div>
       {/if}
 
-      {#if filteredGlobalEnvironment && filteredGlobalEnvironment.length > 0  && localEnvironment.length !== 0 }
+      {#if filteredGlobalEnvironment && filteredGlobalEnvironment.length > 0 && localEnvironment.length !== 0}
         <div class="mb-0">
           <p
             role="button"
@@ -294,7 +304,7 @@
           bind:scrollList
           height={"auto"}
           overflowY={"auto"}
-          classProps={"pe-1"}
+          classProps={"pe-0"}
           style={"flex:1;"}
         >
           {#each filteredLocalEnvironment as env}
