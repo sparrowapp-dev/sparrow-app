9b4e338f13ab71b0fa84e5b4392f02f9d595a844
Md Asif Raza <itsmdasifraza@gmail.com>
2024-09-17 19:02:16 +0530

    feat: integrate run history mock data  [#1401]

--- src/packages/@library/icons/index.ts
+++ src/packages/@library/icons/index.ts
@@ -88,3 +88,5 @@ export { default as ThumbIcon } from "./ThumbIcon.svelte";
 export { default as FilterIcon } from "./FilterIcon.svelte";
 export { default as LinkIcon } from "./LinkIcon.svelte";
 export { default as HistoryIcon } from "./History.svelte";
+export { default as ExclamationIcon } from "./Exclamation.svelte";
+export { default as CheckIcon2 } from "./Check2.svelte";
--- src/packages/@workspaces/features/testflow-explorer/components/run-history/RunHistory.svelte
+++ src/packages/@workspaces/features/testflow-explorer/components/run-history/RunHistory.svelte
@@ -1,9 +1,126 @@
 <script>
-  import { CrossIcon, HistoryIcon } from "@library/icons";
+  import { getMethodStyle } from "$lib/utils/helpers";
+  import {
+    CheckIcon2,
+    CrossIcon,
+    ExclamationIcon,
+    HistoryIcon,
+  } from "@library/icons";
 
   import { Tooltip } from "@library/ui";
   import { WithButtonV5 } from "@workspaces/common/hoc";
-  const timelines = ["", "", "", "", ""];
+
+  const timelines = [
+    {
+      status: "pass",
+      successRequests: "7",
+      failedRequests: "2",
+      totalTIme: "10sec",
+      timestamp: "",
+      requests: [
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "200",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "200",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+      ],
+      expand: true,
+    },
+    {
+      status: "pass",
+      successRequests: "7",
+      failedRequests: "2",
+      totalTIme: "10sec",
+      timestamp: "",
+      requests: [
+        {
+          method: "GET",
+          name: "user login ndfjkebfrfbef refrfbjrefrjfbrerfrf fregferg",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "POST",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login ndfjkebfrfbef refrfbjrefrjfbrerfrf fregferg",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "POST",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+      ],
+      expand: false,
+    },
+    {
+      status: "fail",
+      successRequests: "7",
+      failedRequests: "2",
+      totalTIme: "10sec",
+      timestamp: "",
+      requests: [
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+        {
+          method: "GET",
+          name: "user login",
+          status: "201",
+          time: "1.52ms",
+        },
+      ],
+      expand: true,
+    },
+    ,
+  ];
 </script>
 
 <div class="position-relative">
@@ -19,7 +136,7 @@
       />
     </Tooltip>
     <div
-      class="position-absolute d-flex flex-column bg-tertiary-300 border-radius-2 p-3"
+      class="position-absolute d-flex flex-column bg-tertiary-750 border-radius-2 p-3"
       style="bottom:-10px;
               right:0;
               z-index:100;
@@ -54,16 +171,62 @@
       <!-- BODY -->
       <div style="flex:1; overflow:auto;">
         <div class="time-line ms-3">
-          {#each timelines as tm}
+          {#each timelines as history}
             <div class="position-relative">
-              <div class="m-3 p-3 bg-secondary-400">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum
-                nam min
+              <div class="ms-3 mb-4 p-3 bg-tertiary-670">
+                <div class="d-flex justify-content-between mb-3">
+                  <span class="text-fs-8">
+                    {history?.successRequests} Success | {history?.failedRequests}
+                    fail
+                  </span>
+                  <span class="text-fs-8">{history?.totalTIme}</span>
+                </div>
+                {#each history?.requests as request, index}
+                  {#if index <= 1 || (history.expand && index > 1)}
+                    <div class="d-flex mb-1 align-items-center">
+                      <span
+                        style="padding: 0px 2px; width: 30px;"
+                        class="bg-tertiary-190 text-center text-fs-6-important text-{getMethodStyle(
+                          request.method,
+                        )}">{request.method}</span
+                      >
+                      <span
+                        style="padding: 0px 2px; width: calc(100% - 90px);"
+                        class="px-1 text-fs-8 ellipsis">{request.name}</span
+                      >
+                      <span
+                        style="padding: 0px 2px; width: 20px;"
+                        class="text-fs-8 text-center text-{request.status ===
+                        '201'
+                          ? 'deleteColor'
+                          : 'getColor'}">{request.status}</span
+                      >
+                      <span
+                        style="padding: 0px 2px; width: 40px; text-align: right;"
+                        class="text-fs-8 text-{request.status === '201'
+                          ? 'deleteColor'
+                          : 'getColor'}">{request.time}</span
+                      >
+                    </div>
+                  {/if}
+                {/each}
+                {#if !history.expand && history?.requests.length > 2}
+                  <span class="text-fs-8">see all 8 requests</span>
+                {:else if history.expand && history?.requests.length > 2}
+                  <span class="text-fs-8">see Less</span>
+                {/if}
               </div>
               <div
-                class="position-absolute bg-primary-300"
-                style="height:20px; width:20px; top:0; left: -10px; border-radius:50%; overflow:hidden;"
-              ></div>
+                class="position-absolute bg-tertiary-750 d-flex align-items-center justify-content-center"
+                style="height:14px; width:14px; top:0; left: -8px; border-radius:50%; overflow:hidden;
+                border: 1px solid grey;"
+              >
+                {#if history.status === "pass"}
+                  <CheckIcon2 height={"8px"} width={"8px"} color={"green"} />
+                {:else}
+                  <ExclamationIcon height={"8px"} width={"8px"} color="red" />
+                {/if}
+              </div>
             </div>
           {/each}
         </div>
@@ -74,6 +237,6 @@
 
 <style>
   .time-line {
-    border-left: 1px solid red;
+    border-left: 1px solid grey;
   }
 </style>
