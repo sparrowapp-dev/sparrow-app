3e1555d0e9352f985cd619c986665b195308c9a8
Astitva Patle <ankurpatle18@gmail.com>
2024-09-19 13:03:16 +0530

    fix: added run txt []

--- src/packages/@workspaces/features/testflow-explorer/icons/index.ts
+++ src/packages/@workspaces/features/testflow-explorer/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as ArrowIcon } from "./Arrow.svelte";
 export { default as DropdownArrow } from "./DropdownArrow.svelte";
 export { default as BackArrowIcon } from "./BackArrow.svelte";
+export { default as InfoIcon } from "./Info.svelte";
