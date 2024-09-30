253a804720200f64a35334f054ea76e3071ccd74
Md Asif Raza <itsmdasifraza@gmail.com>
2024-09-30 16:25:07 +0530

    fix:  image imports

--- packages/@sparrow-deprecate/src/assets/icons/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ReloadCollectionIcon } from "@deprecate/assets/icons/reload-collection.svelte";
-export { default as SearchIcon } from "@deprecate/assets/icons/search.svelte";
-export { default as GitBranchIcon } from "@deprecate/assets/icons/git-branch.svelte";
-export { default as PenIcon } from "@deprecate/assets/icons/pen.svelte";
-export { default as DownArrowIcon } from "@deprecate/assets/icons/DownArrow.svelte";
