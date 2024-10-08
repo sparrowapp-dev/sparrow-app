b71ac3f64e444d7b07286c9acf00c9b21a709d0c
Astitva Patle <ankurpatle18@gmail.com>
2024-10-08 14:17:44 +0530

    fix: added welcome popup component []

--- src/packages/@app/pages/Collections/CollectionsPage.svelte
+++ src/packages/@app/pages/Collections/CollectionsPage.svelte
@@ -72,6 +72,7 @@
   import { Button, Modal } from "@library/ui";
   import { isUserFirstSignUp } from "@app/store/user.store";
   import { WelcomeLogo } from "@common/images";
+  import { WelcomePopup } from "@workspaces/features/welcome-popup";
 
   const _viewModel = new CollectionsViewModel();
 
@@ -519,41 +520,17 @@
     isWelcomePopupOpen = false;
   }}
 >
-  <div class="pt-2 pb-4">
-    <div class="d-flex">
-      <WelcomeLogo />
-      <p
-        style="margin-top: 3px; margin-left: 8px; color: var(--text-secondary-100); font-size:18px; font-weight:500;"
-      >
-        Welcome to <span class="gradient-text">Sparrow</span> !
-      </p>
-    </div>
-    <p style="font-size: 16px; color: var(--text-secondary-1000)">
-      Your API journey awaits!<br /> Sparrow app allows you to simplify workflows
-      effortlessly, saving time and effort.
-    </p>
-  </div>
-  <div class="d-flex justify-content-between">
-    <Button
-      title={"Explore Myself"}
-      textStyleProp={"font-size:16px; font-weight:400;"}
-      type={"dark"}
-      onClick={() => {
-        isUserFirstSignUp.set(false);
-        isWelcomePopupOpen = false;
-      }}
-    />
-    <Button
-      title={"Take a Tour"}
-      textStyleProp={"font-size:16px; font-weight:400;"}
-      type={"primary"}
-      onClick={() => {
-        isUserFirstSignUp.set(false);
-        isTourGuideOpen = true;
-        isWelcomePopupOpen = false;
-      }}
-    />
-  </div>
+  <WelcomePopup
+    onClickExplore={() => {
+      isUserFirstSignUp.set(false);
+      isWelcomePopupOpen = false;
+    }}
+    onClickTour={() => {
+      isUserFirstSignUp.set(false);
+      isTourGuideOpen = true;
+      isWelcomePopupOpen = false;
+    }}
+  />
 </Modal>
 
 <svelte:window on:keydown={handleKeyPress} />
