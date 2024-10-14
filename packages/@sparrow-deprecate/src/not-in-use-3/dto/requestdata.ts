fb336ef74229d17bd1094312ae5ea64c8f18da01
testing-techdome <testing.techdome@gmail.com>
2024-10-14 14:21:29 +0530

    Delete packages/@sparrow-deprecate/src/not-in-use-3 directory

--- packages/@sparrow-deprecate/src/not-in-use-3/dto/requestdata.ts
+++
@@ -1,9 +0,0 @@
-import type { ContentTypeEnum } from "../enums/request.enum";
-
-export interface RequestData {
-  body?: object | string;
-  headers?: {
-    Authorization: string;
-    "Content-type"?: ContentTypeEnum;
-  };
-}
