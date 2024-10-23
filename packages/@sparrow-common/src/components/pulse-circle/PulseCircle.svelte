ba74414e31c521a4fc800cddee721038578d000e
Md Asif Raza <itsmdasifraza@gmail.com>
2024-10-23 12:01:25 +0530

    Revert "Revert "Feat: Added tour guide test flow [#1641]""

---
+++ packages/@sparrow-common/src/components/pulse-circle/PulseCircle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let pulsePosition = { top: "0px", left: "0px" }; // Combined position for top and left
+</script>
+
+<!-- Pulse circle element -->
+<div
+  class="pulse-circle"
+  style="top: {pulsePosition.top}; left: {pulsePosition.left};"
+></div>
+
+<style lang="scss">
+  .pulse-circle {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 2px solid #FF663C;
+    background-color: rgba(255, 0, 0, 0.1);
+
+    pointer-events: none; /* Prevents blocking clicks */
+  }
+
+  
+</style>
