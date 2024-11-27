0d14c8fdb750fc74c7c6c8710c465f691ed04e3a
Harsh <harshvardhan@miralabs.ai>
2024-11-27 16:52:21 +0530

    feat: completed ui and the value get stored up in local storage

--- packages/@sparrow-common/src/components/header/Header.svelte
+++ packages/@sparrow-common/src/components/header/Header.svelte
@@ -4,9 +4,11 @@
     CloudOffIcon,
     SparrowEdgeIcon,
     StackIcon,
+    CheckCircle,
   } from "@sparrow/library/icons";
   import { environmentType } from "@sparrow/common/enums";
   import { SparrowIcon } from "@sparrow/library/icons";
+  import { ArrowRightIcon } from "@sparrow/library/icons";
   import constants from "@app/constants/constants";
   import type { WorkspaceDocument } from "@app/database/database";
   import { PlusIcon } from "@sparrow/library/icons";
@@ -71,6 +73,16 @@
     onSwitchWorkspace(tabId);
   };
 
+  let handleAgentDropdown = (tabId: string) => {
+    localStorage.setItem("selectedAgent", tabId);
+    multipleagentvar = tabId;
+  };
+
+  $: {
+    if (multipleagentvar) {
+      localStorage.setItem("selectedAgent", multipleagentvar);
+    }
+  }
   let handleTeamDropdown = (_teamId: string) => {
     onSwitchTeam(_teamId);
   };
@@ -83,6 +95,28 @@
       description: currentTeamName,
     },
   ];
+
+  let multipleAgentData = [
+    {
+      name: "Cloud Agent",
+      id: "Clound Agent",
+      displayName: "Cloud Agent",
+      description:
+        "Run requests via the cloud for faster performance and reduced load on your device.",
+    },
+    {
+      name: "Browser Agent",
+      id: "Browser Agent",
+      displayName: "Browser Agent",
+      description:
+        "Run requests directly from your browser, ideal for local testing.",
+    },
+  ];
+
+  let multipleagentvar = (() => {
+    const storedAgent = localStorage.getItem("selectedAgent");
+    return storedAgent || multipleAgentData[0]?.id;
+  })();
 
   const createSetFromArray = (arr, key) => {
     const seen = new Set();
@@ -356,6 +390,73 @@
       </div>
     {/if}
 
+    <!-- Multiple Agent Dropdown -->
+
+    <Select
+      id={"multiple-agent"}
+      data={multipleAgentData}
+      titleId={`${multipleagentvar}`}
+      onclick={handleAgentDropdown}
+      minHeaderWidth={"232px"}
+      iconRequired={true}
+      icon={CheckCircle}
+      iconColor={"#69D696"}
+      isDropIconFilled={true}
+      borderType={"none"}
+      borderActiveType={"none"}
+      headerHighlight={"hover-active"}
+      headerTheme={"transparent"}
+      menuItem={"v2"}
+      headerFontSize={"12px"}
+      maxHeaderWidth={"12px"}
+      zIndex={200}
+      bodyTheme={"violet"}
+      borderRounded={"2px"}
+      position={"absolute"}
+      isHeaderCombined={false}
+      maxBodyHeight={"300px"}
+    >
+      <div slot="pre-select" class="pre-dropdown">
+        <div class="select-agent">
+          <div>Select Sparrow Agent</div>
+        </div>
+        <div class="upper-underline"></div>
+      </div>
+      <div
+        slot="post-select"
+        class="post-dropdown"
+        style="justify-content: center; align-items:center; flex-direction:row"
+      >
+        <div class="lower-underline"></div>
+        <div class="download-area">
+          <div class="download-sparrow-button dowload-section">
+            <p class="download-text">
+              Download Sparrow Desktop <span class="description">
+                Effortlessly test requests with the desktop app. No agents
+                required.
+              </span>
+            </p>
+          </div>
+          <div class="download-area">
+            <SparrowIcon
+              height="32px"
+              width="32px"
+              color="var(--primary-btn-color)"
+            />
+          </div>
+        </div>
+        <div class="download-btn">
+          <p>Download Now</p>
+          <div>
+            <ArrowRightIcon
+              height="15px"
+              width="11px"
+              color="var(--icon-primary-300)"
+            />
+          </div>
+        </div>
+      </div>
+    </Select>
     <!-- {#if !isWebApp} -->
     <Select
       id={"environment-selector"}
@@ -415,6 +516,12 @@
     height: 44px;
     background-color: var(--bg-secondary-850);
   }
+
+  .description {
+    font-size: 10px;
+    color: var(--text-secondary-200);
+  }
+
   .join-txt {
     font-size: 12px;
     padding-left: 12px;
@@ -443,6 +550,50 @@
     font-weight: 400;
     padding: 10px;
     text-align: center;
+  }
+
+  .download-btn {
+    color: #3670f7;
+    padding: 0 0 0px 12px;
+    font-size: 14px;
+    display: flex;
+    gap: 10px;
+  }
+
+  .dowload-section {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    color: #ffffff;
+    cursor: pointer;
+    font-size: 12px;
+    font-weight: 400;
+    padding: 10px;
+  }
+
+  .select-agent {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    color: #ffffff;
+    cursor: pointer;
+    padding: 10px;
+    font-size: 12px;
+    font-weight: 400;
+  }
+
+  .download-text {
+    margin: 0;
+  }
+  .download-area {
+    display: flex;
+    justify-content: center;
+    align-self: center;
+  }
+
+  .download-sparrow-button {
+    display: flex;
+    flex-direction: row;
   }
 
   .create-new-workspace {
