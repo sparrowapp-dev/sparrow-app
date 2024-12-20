26b1464ee0ae53490c0051b970aaa2b0bfff8e84
Astitva Patle <ankurpatle18@gmail.com>
2024-12-20 18:21:13 +0530

    feat: added team members and settings tab []

--- packages/@sparrow-teams/src/features/team-explorer/components/team-navigator/TeamNavigator.svelte
+++ packages/@sparrow-teams/src/features/team-explorer/components/team-navigator/TeamNavigator.svelte
@@ -3,70 +3,42 @@
   export let tabs = [];
   export let activeTeamTab: string;
   export let onUpdateActiveTab: (data: string) => void;
-  export let isWebApp;
 </script>
 
-{#if isWebApp}
-  <div class="">
-    <!-- Tabs -->
-    <div class="d-flex mb-2">
-      {#each tabs as tab}
-        {#if tab?.visible && tab.id === activeTeamTab}
-          <button
-            class="navigation__link-web border-0 sparrow-fs-12 request-tab pb-2 me-3"
-            role="tab"
-          >
-            <span
-              class="d-flex align-items-center text-fs-16"
-              style="font-weight:700"
-              ><span>{tab.name}</span>
-              {#if tab.count}
-                <span class="ms-1"></span>
-                <Label number={tab.count} size={"18px"} />
-              {/if}
-            </span>
-          </button>
-        {/if}
-      {/each}
-    </div>
+<div class="">
+  <!-- Tabs -->
+  <div class="d-flex mb-2">
+    {#each tabs as tab}
+      {#if tab?.visible}
+        <button
+          class="navigation__link border-0 sparrow-fs-12 request-tab me-3 {tab.id ===
+          activeTeamTab
+            ? 'tab-active'
+            : ''} {tab.disabled ? 'tab-disabled' : ''}"
+          role="tab"
+          on:click={() => {
+            if (!tab.disabled) {
+              onUpdateActiveTab(tab.id);
+            }
+          }}
+          disabled={tab.disabled}
+        >
+          <span
+            class="d-flex align-items-center ps-1 pe-1 {tab.id !== activeTeamTab
+              ? 'tab-active-text'
+              : ''}"
+            style="font-size: 12px; font-weight:700"
+            ><span>{tab.name}</span>
+            {#if tab.count}
+              <span class="ms-1"></span>
+              <Label number={tab.count} />
+            {/if}
+          </span>
+        </button>
+      {/if}
+    {/each}
   </div>
-{:else}
-  <div class="">
-    <!-- Tabs -->
-    <div class="d-flex mb-2">
-      {#each tabs as tab}
-        {#if tab?.visible}
-          <button
-            class="navigation__link border-0 sparrow-fs-12 request-tab me-3 {tab.id ===
-            activeTeamTab
-              ? 'tab-active'
-              : ''} {tab.disabled ? 'tab-disabled' : ''}"
-            role="tab"
-            on:click={() => {
-              if (!tab.disabled) {
-                onUpdateActiveTab(tab.id);
-              }
-            }}
-            disabled={tab.disabled}
-          >
-            <span
-              class="d-flex align-items-center ps-1 pe-1 {tab.id !==
-              activeTeamTab
-                ? 'tab-active-text'
-                : ''}"
-              style="font-size: 12px; font-weight:700"
-              ><span>{tab.name}</span>
-              {#if tab.count}
-                <span class="ms-1"></span>
-                <Label number={tab.count} />
-              {/if}
-            </span>
-          </button>
-        {/if}
-      {/each}
-    </div>
-  </div>
-{/if}
+</div>
 
 <style>
   .navigation__link {
