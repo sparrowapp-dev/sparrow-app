fc2588ccbb1364cc515217ea406caf3834564e6e
Md Asif Raza <itsmdasifraza@gmail.com>
2025-01-12 17:04:33 +0530

    fe

--- apps/@sparrow-desktop/src/adapter/graphql-tab.ts
+++ apps/@sparrow-desktop/src/adapter/graphql-tab.ts
@@ -1,4 +1,4 @@
-import { createDeepCopyad } from "@sparrow/common/utils";
+import { createDeepCopyfad } from "@sparrow/common/utils";
 import { type Path, type Tab } from "@sparrow/common/types/workspace/tab";
 import { InitRequestTab } from "@sparrow/common/utils";
 import { InitTab } from "@sparrow/common/factory";
