0e0d76f21b9591929daf25ac4e0a267daebe89be
Mahendra <mahendra09022001@gmail.com>
2025-01-15 12:55:40 +0530

    feat/storybook -spacing removing the unwanted data

--- packages/@sparrow-library/src/ui/spacing/HtmlRenderer.svelte
+++ packages/@sparrow-library/src/ui/spacing/HtmlRenderer.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-  import { marked } from "marked";
-
-  export let data: string = ""; // Markdown data to render
-  export let padding: string = "16px"; // Default padding
-  export let margin: string = "0"; // Default margin
+  export let data: string = "";
 </script>
 
 <div>{@html data}</div>
