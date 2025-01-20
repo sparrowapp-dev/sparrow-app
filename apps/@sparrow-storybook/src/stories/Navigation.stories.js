2f5a61af89b650ae6171eb6345556fea05643972
Mahendra <mahendra09022001@gmail.com>
2025-01-20 16:01:53 +0530

    storybook navigation comopnent

---
+++ apps/@sparrow-storybook/src/stories/Navigation.stories.js
@@ -0,0 +1,19 @@
+// import { Navigation } from "@sparrow/library/ui";
+
+import {
+    Sidebar,
+  } from "@sparrow/common/components";
+
+export default {
+  title: 'Components/Navigation',
+  component: Sidebar,
+  tags: ['autodocs'],
+  
+ 
+}
+
+export const primary = {
+    args:{
+    
+    },
+}
