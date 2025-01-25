1732db582af1d7f57dda540bd4cb860568921bc0
Md Asif Raza <itsmdasifraza@gmail.com>
2025-01-26 04:58:05 +0530

    fix: update eslint config [#100]

--- apps/@sparrow-desktop/vite.config.ts
+++ apps/@sparrow-desktop/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    eslintPlugin({
+      fix: false, // Auto-fix linting errors
+      emitError: true, // Emit errors on the console (terminal)
+    }),
+  ],
   resolve: {
     alias: {
       "@app": path.resolve("./src/"),
--- apps/@sparrow-storybook/vite.config.js
+++ apps/@sparrow-storybook/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    eslintPlugin({
+      fix: false, // Auto-fix linting errors
+      emitError: true, // Emit errors on the console (terminal)
+    }),
+  ],
+});
