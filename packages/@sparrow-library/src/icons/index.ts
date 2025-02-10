16bbfe734eb2f54228a9ce9e59e68f0fd6decc60
Mahendra <mahendra09022001@gmail.com>
2025-02-10 14:37:49 +0530

    ds-input-search

--- packages/@sparrow-library/src/icons/index.ts
+++ packages/@sparrow-library/src/icons/index.ts
@@ -125,3 +125,6 @@ export { default as AiSparkleIconV3 } from "./AiSparkleV3.svelte";
 
 export {default as Search  } from "./SearchIcon.svelte";
 export {default as DismissRegular} from "./DismissRegular.svelte";
+
+export {default as SearchIcon} from "./SearchIcon.svelte";
+export {default as crossIcon} from "./crossIcon.svelte";
