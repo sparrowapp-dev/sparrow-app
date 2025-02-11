f1ee7dc5ff9dbd4ec110c7d6dbea3f778bd44c2b
Mahendra <mahendra09022001@gmail.com>
2025-02-11 11:18:13 +0530

    updates

--- packages/@sparrow-library/src/ui/index.ts
+++ packages/@sparrow-library/src/ui/index.ts
@@ -16,5 +16,5 @@ export * from "./icon-fallback";
 export * from "./toast";
 export * from "./image-modal";
 export * from "./skeleton";
-export * from "./Navigation";
+export * from "./Navigator";
 export * from "./badge";
