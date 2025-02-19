53e26c0792e3ee9861e400a20347877b6e734c06
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-02-19 22:07:15 +0530

    fix: updated the radio button size

--- packages/@sparrow-library/src/ui/radio-button/icons/selectedIcon.svelte
+++ packages/@sparrow-library/src/ui/radio-button/icons/selectedIcon.svelte
@@ -2,17 +2,41 @@
   export let width: number = 16;
   export let height: number = 16;
   export let selectedColor: string = "#6894F9";
+  export let classProp: string = "";
 </script>
 
-<svg
-  {width}
-  {height}
-  viewBox="0 0 12 12"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path
-    d="M6 9C7.65685 9 9 7.65685 9 6C9 4.34315 7.65685 3 6 3C4.34315 3 3 4.34315 3 6C3 7.65685 4.34315 9 6 9ZM6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0ZM1 6C1 3.23858 3.23858 1 6 1C8.76142 1 11 3.23858 11 6C11 8.76142 8.76142 11 6 11C3.23858 11 1 8.76142 1 6Z"
-    fill={selectedColor}
-  />
-</svg>
+{#if width === 16 && height === 16}
+  <svg
+    {width}
+    {height}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="{classProp}  select-icon"
+  >
+    <path
+      d="M8 13C10.7614 13 13 10.7614 13 8C13 5.23858 10.7614 3 8 3C5.23858 3 3 5.23858 3 8C3 10.7614 5.23858 13 8 13ZM8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0ZM1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8Z"
+      fill={selectedColor}
+    />
+  </svg>
+{:else}
+  <svg
+    {width}
+    {height}
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="{classProp}  select-icon"
+  >
+    <path
+      d="M6 9C7.65685 9 9 7.65685 9 6C9 4.34315 7.65685 3 6 3C4.34315 3 3 4.34315 3 6C3 7.65685 4.34315 9 6 9ZM6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0ZM1 6C1 3.23858 3.23858 1 6 1C8.76142 1 11 3.23858 11 6C11 8.76142 8.76142 11 6 11C3.23858 11 1 8.76142 1 6Z"
+      fill={selectedColor}
+    />
+  </svg>
+{/if}
+
+<style>
+  .select-icon {
+    cursor: pointer;
+  }
+</style>
--- packages/@sparrow-library/src/ui/radio-button/icons/unSelectedIcon.svelte
+++ packages/@sparrow-library/src/ui/radio-button/icons/unSelectedIcon.svelte
@@ -5,23 +5,38 @@
   export let unSelectedColor: string = "#B6B7B9";
 </script>
 
-<svg
-  {width}
-  {height}
-  viewBox="0 0 12 12"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  class="{classProp} select-icon"
->
-  <path
-    d="M6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1ZM0 6C0 2.68629 2.68629 0 6 0C9.31371 0 12 2.68629 12 6C12 9.31371 9.31371 12 6 12C2.68629 12 0 9.31371 0 6Z"
-    fill={unSelectedColor}
-  />
-</svg>
+{#if width === 16 && height === 16}
+  <svg
+    {width}
+    {height}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="{classProp} select-icon"
+  >
+    <path
+      d="M8 1C4.13401 1 1 4.13401 1 8C1 11.866 4.13401 15 8 15C11.866 15 15 11.866 15 8C15 4.13401 11.866 1 8 1ZM0 8C0 3.58172 3.58172 0 8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8Z"
+      fill={unSelectedColor}
+    />
+  </svg>
+{:else}
+  <svg
+    {width}
+    {height}
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="{classProp} select-icon"
+  >
+    <path
+      d="M6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1ZM0 6C0 2.68629 2.68629 0 6 0C9.31371 0 12 2.68629 12 6C12 9.31371 9.31371 12 6 12C2.68629 12 0 9.31371 0 6Z"
+      fill={unSelectedColor}
+    />
+  </svg>
+{/if}
 
 <style>
   .select-icon {
-    padding: 3px;
     cursor: pointer;
   }
 </style>
