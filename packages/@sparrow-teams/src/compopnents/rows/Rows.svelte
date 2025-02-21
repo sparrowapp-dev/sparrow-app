28932437de2134b584a40b5667897b2fa57b89bf
disha <dishachhabra173@gmail.com>
2025-02-21 16:48:02 +0530

    dropdown comp added

--- packages/@sparrow-teams/src/compopnents/rows/Rows.svelte
+++ packages/@sparrow-teams/src/compopnents/rows/Rows.svelte
@@ -3,6 +3,8 @@
   import { UserProfileList } from "@sparrow/teams/compopnents";
   import { MenuView } from "@sparrow/teams/compopnents";
   import { TeamRole, WorkspaceMemberRole } from "@sparrow/common/enums";
+  import { Button } from "@sparrow/library/ui";
+  import { Dropdown } from "@sparrow/library/ui";
 
   export let list;
   export let activeTeam;
@@ -38,46 +40,53 @@
     }, 100);
   };
 
+  let isDropdownVisible = false;
+  let isDropdownHovered = false;
+  
+  let dropdownOptions = [];
+  
   $: {
     if (isAdminOrOwner) {
-      menuItems = [
+      dropdownOptions = [
         {
-          onClick: () => {
-            onOpenCollection(list._id);
-          },
-          displayText: "Open Workspace",
-          disabled: false,
+          name: "Open Workspace",
+          color: "var(--text-secondary-100)",
+          onclick: () => onOpenCollection(list._id),
         },
         {
-          onClick: () => {
-            onAddMember({
-              workspaceID: list._id,
-              workspaceName: list.name,
-              users: list.users,
-            });
-          },
-          displayText: "Add Members",
-          disabled: false,
+          name: "Add Members",
+          color: "var(--text-secondary-100)",
+          onclick: () => onAddMember({
+            workspaceID: list._id,
+            workspaceName: list.name,
+            users: list.users,
+          }),
         },
         {
-          onClick: () => {
-            onDeleteWorkspace(list);
-          },
-          displayText: "Delete Workspace",
-          disabled: false,
+          name: "Delete Workspace",
+          color: "var(--text-secondary-100)",
+          onclick: () => onDeleteWorkspace(list),
         },
       ];
     } else {
-      menuItems = [
+      dropdownOptions = [
         {
-          onClick: () => {
-            onOpenCollection(list._id);
-          },
-          displayText: "Open Workspace",
-          disabled: false,
+          name: "Open Workspace",
+          color: "var(--text-secondary-100)",
+          onclick: () => onOpenCollection(list._id),
         },
       ];
     }
+  }
+
+  function getDropdownPosition(buttonElement: HTMLElement) {
+    if (!buttonElement) return { top: 0, left: 0 };
+    
+    const rect = buttonElement.getBoundingClientRect();
+    const top = rect.bottom + 5; // 5px offset from button
+    const left = rect.left;      // Align with button's left edge
+    
+    return { top, left };
   }
 
   function closeRightClickContextMenu() {
@@ -86,7 +95,7 @@
 </script>
 
 {#if showMenu}
-  <MenuView xAxis={pos.x} yAxis={pos.y} {noOfRows} {noOfColumns} {menuItems} />
+  <!-- <MenuView xAxis={pos.x} yAxis={pos.y} {noOfRows} {noOfColumns} {menuItems} /> -->
 {/if}
 
 <svelte:window
@@ -105,7 +114,7 @@
       onOpenCollection(list._id);
     }}
     style="max-width: 15vw; padding-right: 10px;"
-    class="tab-data rounded-start py-3 overflow-hidden ellipsis"
+    class="tab-data rounded-start py-3  overflow-hidden ellipsis"
   >
     {list?.name}
     {#if list?.isNewInvite}
@@ -134,7 +143,7 @@
       }}
       class="tab-data py-3"
     >
-      <div class="d-flex">
+      <div class="d-flex px-3">
         <UserProfileList
           width={24}
           height={25}
@@ -176,18 +185,42 @@
 
   <td class="tab-data rounded-end py-3"
   >
-    <button
-      bind:this={workspaceTabWrapper}
-      class="threedot-icon-container border-0 rounded d-flex justify-content-center align-items-center position-relative {showMenu
-        ? 'threedot-active'
-        : ''}"
-      on:click={(e) => {
-        rightClickContextMenu(e);
-      }}
-      tabindex="-1"
-    >
-      <ThreeDotIcon />
-    </button>
+  <div class="threedot-icon-container" style="display: flex; justify-content: center; align-items: center;">
+      <Dropdown
+        buttonId="workspace-dropdown-{list._id}"
+        bind:isMenuOpen={isDropdownVisible}
+        horizontalPosition="right"
+        minWidth={175}
+        options={dropdownOptions}
+        menuPosition={getDropdownPosition(document.getElementById(`workspace-dropdown-{list._id}`))}
+      >
+        <div style="transform: rotate(90deg);">
+          <button
+            on:mouseenter={() => {
+              isDropdownHovered = true;
+            }}
+            on:mouseleave={() => {
+              isDropdownHovered = false;
+            }}
+            id="workspace-dropdown-{list._id}"
+            class="border-0 pt-0 border-radius-4"
+            style="background-color: transparent; height:24px; width:24px;"
+            on:click={() => {
+              isDropdownVisible = !isDropdownVisible;
+            }}
+          >
+            <ThreeDotIcon
+              color={isDropdownVisible || isDropdownHovered
+                ? "var(--icon-ds-primary-300)"
+                : "var(--icon-secondary-100)"}
+              width="16px"
+              height="16px"
+            />
+          </button>
+        </div>
+      </Dropdown>
+    </div>
+
   </td>
 </tr>
 
