f9e5d6913f6f2856e216c1fc523916b3e3ac4753
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-02-25 12:31:04 +0530

    feat: updated icon

--- packages/@sparrow-library/src/icons/ChevronUpRegular.svelte
+++ packages/@sparrow-library/src/icons/ChevronUpRegular.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <i
-  class="icon-ic_fluent_delete_20_regular"
+  class="icon-ic_fluent_chevron_up_20_filled"
   style="font-size:{size}; color:{finalColor}; display: flex; justify-content:center; align-items:center;"
 ></i>
 
