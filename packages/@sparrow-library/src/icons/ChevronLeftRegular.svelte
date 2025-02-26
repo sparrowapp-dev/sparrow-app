0c9b70212b496778d2abbfa73a0eec824012a962
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-02-26 18:24:31 +0530

    feat : updated icon component

--- packages/@sparrow-library/src/icons/ChevronLeftRegular.svelte
+++ packages/@sparrow-library/src/icons/ChevronLeftRegular.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
   export let size = "20px";
-  export let color = "#ffffff";
+  export let color = "";
+  $: finalColor = color === "" ? "inherit" : color;
 </script>
 
 <i
   class="icon-ic_fluent_chevron_left_20_filled"
-  style="font-size:{size}; color:{color}; display: flex; justify-content:center; align-items:center;"
-></i>
-
-<style>
-</style>
+  style="font-size:{size}; color:{finalColor}; display: flex; justify-content:center; align-items:center;"
+>
+</i>
