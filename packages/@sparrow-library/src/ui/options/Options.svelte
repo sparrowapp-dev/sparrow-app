bcd6c83836df322dd3013933ae2ba6ee6e890981
disha <dishachhabra173@gmail.com>
2025-03-06 17:03:40 +0530

    reverted changes in options.svelte

--- packages/@sparrow-library/src/ui/options/Options.svelte
+++ packages/@sparrow-library/src/ui/options/Options.svelte
@@ -104,7 +104,6 @@
     -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
     -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
     box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
-    background-color: var(--bg-ds-surface-600);
   }
 
   ul li button {
