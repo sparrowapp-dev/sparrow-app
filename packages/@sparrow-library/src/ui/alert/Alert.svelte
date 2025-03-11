abe96349f97ea488bf2f2045249bca8bf580cef4
Mahendra <mahendra09022001@gmail.com>
2025-03-11 22:13:46 +0530

    alert comoponent

--- packages/@sparrow-library/src/ui/alert/Alert.svelte
+++ packages/@sparrow-library/src/ui/alert/Alert.svelte
@@ -39,13 +39,14 @@
       style=" gap:16px; padding:16px;  "
     >
       <div
-        style="height: 100%; width: 32px; display:flex; align-items:center; justify-content:center;"
+        style="height: 100%; width: 32px; display:flex; align-items:center; justify-content:center; "
       >
         <div
           class="absolute inset-0 opacity-40 rounded-full"
           style="background-color:var(--bg-ds-surface-100); border-radius:50%; height:32px; width:32px; display:flex; align-items:center; justify-content:center;"
         >
           {#if varient === "error"}
+            <span class="icon-backdrop-error" />
             <ErrorCircleFilled color={"var(--bg-ds-danger-400)"} />
           {:else if varient === "success"}
             <CheckmarkCircleFilled color={"var(--icon-ds-success-400)"} />
@@ -79,6 +80,7 @@
 
 <style lang="scss">
   .errorClass {
+    position: relative;
     border-radius: 6px;
     border: 2px solid var(--bg-ds-danger-400);
     border-right-width: 0;
@@ -106,5 +108,67 @@
     border-right-width: 0;
     border-top-width: 0;
     border-bottom-width: 0;
+  }
+
+  .icon-backdrop-error {
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    z-index: 1;
+    top: 50%;
+    left: 7%;
+    transform: translate(-50%, -50%);
+    background: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(235, 86, 81, 0.25) 0%,
+      /* Increased opacity from 0.13 to 0.25 */ rgba(235, 86, 81, 0.1) 60%,
+      /* Added middle stop with some opacity */ rgba(29, 33, 43, 0) 100%
+    );
+  }
+  .icon-backdrop-success {
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    z-index: 1;
+    top: 50%;
+    left: 7%;
+    transform: translate(-50%, -50%);
+    background: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(51, 204, 122, 0.12) 0%,
+      rgba(29, 33, 43, 0) 100%
+    );
+  }
+  .icon-backdrop-info {
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    z-index: 1;
+    top: 50%;
+    left: 7%;
+    transform: translate(-50%, -50%);
+    background: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(51, 204, 122, 0.12) 0%,
+      rgba(29, 33, 43, 0) 100%
+    );
+  }
+  .icon-backdrop-warning {
+    position: absolute;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    z-index: 1;
+    top: 50%;
+    left: 7%;
+    transform: translate(-50%, -50%);
+    background: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(236, 197, 81, 0.13) 0%,
+      rgba(29, 33, 43, 0) 100%
+    );
   }
 </style>
