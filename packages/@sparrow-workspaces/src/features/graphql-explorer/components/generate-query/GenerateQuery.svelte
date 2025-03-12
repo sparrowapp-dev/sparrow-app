382c4ce22ccb7c982f23c7755c00f6f768095364
Mahendra <mahendra09022001@gmail.com>
2025-03-12 12:37:52 +0530

    updates

--- packages/@sparrow-workspaces/src/features/graphql-explorer/components/generate-query/GenerateQuery.svelte
+++ packages/@sparrow-workspaces/src/features/graphql-explorer/components/generate-query/GenerateQuery.svelte
@@ -7,7 +7,7 @@
     ThreeDotIcon,
   } from "@sparrow/library/icons";
   import { trashIcon } from "@sparrow/library/assets";
-  import { Search } from "@sparrow/library/forms";
+  import { Input, Search } from "@sparrow/library/forms";
   import { Checkbox } from "@sparrow/library/forms";
   import { Button } from "@sparrow/library/ui";
   import { GraphqlRequestOperationTabEnum } from "@sparrow/common/types/workspace/graphql-request-tab";
@@ -774,7 +774,10 @@
                       class="input-parent pe-2 mb-2 position-relative"
                       style="padding-left: 35px;"
                     >
-                      <input
+                      <Input
+                        id={t.id}
+                        variant="stroke"
+                        size="small"
                         type="text"
                         style="border:1px solid grey; outline:none;"
                         class="arg-input w-100 bg-transparent border-radius-2 px-2 pe-3 py-1 text-fs-12"
