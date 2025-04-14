472116c0d72c69aea2648e2d1505e2eb24fa8a43
kishangupta4514 <kishangupta4514@gmail.com>
2025-04-14 12:55:14 +0530

    fix: text-size in delete pop-up

--- packages/@sparrow-workspaces/src/features/collection-list/components/saved-request/SavedRequest.svelte
+++ packages/@sparrow-workspaces/src/features/collection-list/components/saved-request/SavedRequest.svelte
@@ -130,7 +130,7 @@
   isOpen={isDeletePopup}
   handleModalState={() => (isDeletePopup = false)}
 >
-  <div class="text-lightGray mb-1 text-ds-font-size-14">
+  <div class="text-lightGray mb-1 text-ds-font-size-14 text-ds-font-weight-medium">
     <p>
       Are you sure you want to delete this response? <span
         class="text-whiteColor fw-bold">"{api.name}"</span
