832bea06d78208688f9d8b2da77da105f48b3fb1
disha <dishachhabra173@gmail.com>
2025-04-18 18:31:03 +0530

    done in saved response too

--- packages/@sparrow-workspaces/src/features/rest-explorer/components/http-url-section/HttpUrlSection.svelte
+++ packages/@sparrow-workspaces/src/features/rest-explorer/components/http-url-section/HttpUrlSection.svelte
@@ -121,20 +121,6 @@
     menuItem={"v2"}
     highlightTickedItem={false}
   />
-
-  <!-- <CodeMirrorInput
-    value={requestUrl}
-    onUpdateInput={onUpdateRequestUrl}
-    placeholder={"Enter a URL"}
-    {theme}
-    {onUpdateEnvironment}
-    {environmentVariables}
-    codeId={"url"}
-    class={"input-url"}
-    {userRole}
-    isFocusedOnMount={true}
-  /> -->
-
   <div class="w-100 d-flex align-items-center position-relative ">
     <div class="position-absolute top-0" style="width: calc(100% );">
       <CodeMirrorInput
