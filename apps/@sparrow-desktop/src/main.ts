6627d1ea52981e00e63bc3beb3b8c2699a23ec8d
kishangupta4514 <kishangupta4514@gmail.com>
2025-04-28 16:56:14 +0530

    add sentry condition for different environments

--- apps/@sparrow-desktop/src/main.ts
+++ apps/@sparrow-desktop/src/main.ts
@@ -23,7 +23,7 @@ const formatVersion = (version: string): string => {
 
 
 // // Initialize Sentry
-if (constants.APP_ENVIRONMENT == 'LOCAL-FE') {
+if (constants.APP_ENVIRONMENT !== 'LOCAL-FE') {
   Sentry.init({
     dsn: constants.SENTRY_DSN,
     environment: constants.APP_ENVIRONMENT, // Set the environment
