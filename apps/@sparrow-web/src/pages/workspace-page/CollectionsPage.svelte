c789905f0e4274fd5ae9823152cf55a599f9573e
Sai Charan Kamble <sai.charan.kamble@techdome.net.in>
2025-04-28 14:54:03 +0530

    add/document_response

--- apps/@sparrow-web/src/pages/workspace-page/CollectionsPage.svelte
+++ apps/@sparrow-web/src/pages/workspace-page/CollectionsPage.svelte
@@ -2,7 +2,7 @@
   import { Route } from "svelte-navigator";
   import { Pane, Splitpanes } from "svelte-splitpanes";
   import { userValidationStore } from "@app/store/deviceSync.store";
-
+  import { captureEvent } from "@app/utils/posthog/posthogConfig";
   // ---- Store
   import {
     leftPanelWidth,
@@ -339,6 +339,19 @@
   /**
    * Handle save functionality on close confirmation popup
    */
+
+  const handlecollection_document_response = ({
+    event_name,
+  }: {
+    event_name: string;
+  }) => {
+    captureEvent("document_response", {
+      component: "CollectionsPage",
+      button_text: event_name,
+      destination: event_name,
+    });
+  };
+
   const handlePopupSave = async () => {
     if (
       removeTab.type === TabTypeEnum.ENVIRONMENT ||
@@ -1009,6 +1022,9 @@
       customWidth={"95px"}
       disable={userRole === WorkspaceRole.WORKSPACE_VIEWER}
       onClick={() => {
+        handlecollection_document_response({
+          event_name: "Response Doc Saved!",
+        });
         handlePopupSave();
       }}
     ></Button>
