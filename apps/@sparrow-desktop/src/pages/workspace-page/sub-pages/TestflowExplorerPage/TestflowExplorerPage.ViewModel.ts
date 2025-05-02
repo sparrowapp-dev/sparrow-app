26a47d2cf52f1072a8a32f025c19f2462a943d0d
Md Asif Raza <itsmdasifraza@gmail.com>
2025-05-02 13:57:07 +0530

    feat: shift block name with request name

--- apps/@sparrow-desktop/src/pages/workspace-page/sub-pages/TestflowExplorerPage/TestflowExplorerPage.ViewModel.ts
+++ apps/@sparrow-desktop/src/pages/workspace-page/sub-pages/TestflowExplorerPage/TestflowExplorerPage.ViewModel.ts
@@ -598,7 +598,8 @@ export class TestflowExplorerPageViewModel {
                 else{
                   reqBody = decodeData[3];
                 }
-                requestChainResponse[element.data.blockName] = {
+                // debugger;
+                requestChainResponse["$$" + element.data.requestData.name.replace(/[^a-zA-Z0-9_]/g, "_")] = {
                   response: {
                     body: responseHeader === "JSON" ? JSON.parse(resData.body) : resData.body,
                     headers: response?.data?.headers
@@ -643,7 +644,7 @@ export class TestflowExplorerPageViewModel {
                 request: adaptedRequest,
               });
 
-              requestChainResponse[element.data.blockName] = {
+              requestChainResponse["$$" + element.data.requestData.name.replace(/[^a-zA-Z0-9_]/g, "_")] = {
                 response: {
                   body: {},
                   headers:{}
