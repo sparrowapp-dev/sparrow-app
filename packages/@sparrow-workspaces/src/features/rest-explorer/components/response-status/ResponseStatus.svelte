b65295241e184fc704f5222c655d8b22b2bf02fd
Sai Charan Kamble <sai.charan.kamble@techdome.net.in>
2025-05-05 18:50:26 +0530

    fix: tooltip-position

--- packages/@sparrow-workspaces/src/features/rest-explorer/components/response-status/ResponseStatus.svelte
+++ packages/@sparrow-workspaces/src/features/rest-explorer/components/response-status/ResponseStatus.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { ResponseStatusCode } from "@sparrow/common/enums/request.enum";
-  import { ArchiveRegular, BoxIcon, ClockIcon, ClockRegular, DotIcon } from "@sparrow/library/icons";
+  import {
+    ArchiveRegular,
+    BoxIcon,
+    ClockIcon,
+    ClockRegular,
+    DotIcon,
+  } from "@sparrow/library/icons";
   import type { Response } from "@sparrow/common/types/workspace";
   import { Tooltip } from "@sparrow/library/ui";
   export let response: Response;
@@ -20,20 +26,20 @@
   };
 </script>
 
-<div
-  class="d-flex flex-column align-items-start justify-content-between w-100"
-  
->
+<div class="d-flex flex-column align-items-start justify-content-between w-100">
   <div
     class="response-container d-flex align-items-center justify-content-between z-1 p-2 w-100 position-sticky border-radius-2"
     style="top:55.4px;  margin-top: -1px;"
   >
-    <div class="d-flex align-items-center gap-2 ms-auto" style="justify-content: right;">
+    <div
+      class="d-flex align-items-center gap-2 ms-auto"
+      style="justify-content: right;"
+    >
       <!-- insert controller here -->
       <div class="d-flex align-items-center gap-3">
         <Tooltip
           title="HTTP Status - {response.status}"
-          placement={"bottom-center"}
+          placement={"top-center"}
           zIndex={500}
         >
           <span
@@ -46,7 +52,7 @@
                 ? 'var(--text-ds-success-300)'
                 : 'var(--request-delete)'};"
             >
-              <span class="d-flex" >
+              <span class="d-flex">
                 <DotIcon
                   color={checkIfRequestSucceed(response?.status)
                     ? "var(--text-ds-success-300)"
@@ -59,11 +65,7 @@
             >
           </span>
         </Tooltip>
-        <Tooltip
-          title="Time"
-          placement={"bottom-center"}
-          zIndex={500}
-        >
+        <Tooltip title="Time" placement={"top-center"} zIndex={500}>
           <span
             class="text-fs-12 d-flex align-items-center border-0 justify-content-center align-items-center rounded time-primary1 response-text"
             style=" color:{checkIfRequestSucceed(response?.status)
@@ -71,13 +73,18 @@
               : 'var(--request-delete)'};"
           >
             <span class="d-flex">
-              <ClockRegular size="12px" color={checkIfRequestSucceed(response?.status)
+              <ClockRegular
+                size="12px"
+                color={checkIfRequestSucceed(response?.status)
                   ? "var(--text-ds-success-300)"
-                  : "var(--request-delete)"}/>
+                  : "var(--request-delete)"}
+              />
             </span>
             <span
-              class="text-fs-12 "
-              style=" margin-left:6px;color:{checkIfRequestSucceed(response?.status)
+              class="text-fs-12"
+              style=" margin-left:6px;color:{checkIfRequestSucceed(
+                response?.status,
+              )
                 ? 'var(--text-ds-success-300)'
                 : 'var(--request-delete)'};"
             >
@@ -95,17 +102,13 @@
             </p>
           </span>
         </Tooltip>
-        <Tooltip
-          title="Size"
-          placement={"bottom-center"}
-          zIndex={500}
-        >
+        <Tooltip title="Size" placement={"top-center"} zIndex={500}>
           <span
             class="d-flex align-items-center justify-content-center rounded border-0 text-backgroundColor size-primary1 response-text"
             style="font-size: 12px;"
           >
             <span class="d-flex" style="margin-right: 6px;">
-              <ArchiveRegular size={"12px"} color="var(--icon-ds-neutral-50)"/>
+              <ArchiveRegular size={"12px"} color="var(--icon-ds-neutral-50)" />
             </span>
             {response.size?.toFixed(2)}
             <p class="mb-0 ms-1" style="font-size: 12px;">KB</p>
@@ -132,7 +135,7 @@
   .statuscode {
     background-color: transparent;
   }
-  .response-text{
+  .response-text {
     font-weight: 500;
   }
 </style>
