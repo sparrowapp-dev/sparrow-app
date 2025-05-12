12ecf85454defcdaa0fa9e8fd08dbc9e29c5e959
amitpawar <amitpawar7475@gmail.com>
2025-05-12 17:44:53 +0530

    fix: database stack icon

--- packages/@sparrow-library/src/icons/DatabaseStackRegular.svelte
+++ packages/@sparrow-library/src/icons/DatabaseStackRegular.svelte
@@ -1,13 +1,18 @@
-<script lang="ts">
-  export let size = "20px";
-  export let color = "";
-  $: finalColor = color === "" ? "inherit" : color;
+<script>
+  export let height = "14px";
+  export let width = "10px";
+  export let color = "grey";
 </script>
 
-<i
-  class="icon-ic_fluent_database_stack_16_regular"
-  style="font-size:{size}; color:{finalColor}; display: flex; justify-content:center; align-items:center;"
-></i>
-
-<style>
-</style>
+<svg
+  {width}
+  {height}
+  viewBox="0 0 10 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M5 0C2.149 0 0 1.075 0 2.5V11.5C0 12.925 2.149 14 5 14C7.851 14 10 12.925 10 11.5V2.5C10 1.075 7.851 0 5 0ZM5 1C7.441 1 9 1.888 9 2.5C9 3.112 7.441 4 5 4C2.559 4 1 3.112 1 2.5C1 1.888 2.558 1 5 1ZM5 13C2.558 13 1 12.111 1 11.5V8.537C1.897 9.13 3.328 9.5 5 9.5C6.672 9.5 8.103 9.13 9 8.537V11.5C9 12.111 7.441 13 5 13ZM5 8.5C2.558 8.5 1 7.612 1 7V4.021C2.214 4.719 3.601 5.058 5 5C6.399 5.058 7.786 4.719 9 4.021V7C9 7.612 7.442 8.5 5 8.5Z"
+    fill={color}
+  />
+</svg>
