6c49d7b730d8d6cec272898ed02504ca60325046
kishangupta4514 <kishangupta4514@gmail.com>
2025-05-15 23:18:27 +0530

    fix/workspace-invite-bugs

--- packages/@sparrow-common/src/features/create-team/constants/icon.ts
+++ packages/@sparrow-common/src/features/create-team/constants/icon.ts
@@ -1,6 +1,6 @@
 export const ICON_CONFIG = {
   TITLE: `Upload Logo`,
-  DESCRIPTION: `Drag and drop your image here. For the best results, upload a square image (recommended). The file size must not exceed 2 MB.`,
+  DESCRIPTION: `Upload your image (max 2 MB, square format). Supported: .jpg, .jpeg, .png.`,
   FILE_TYPES: [".jpg", ".jpeg", ".png"],
   MAX_FILE_SIZE_KB: 2048,
   SIZE_EXCEED_ERROR_MESSAGE: `The size of the file you are trying to upload is more than 2 MB.`,
