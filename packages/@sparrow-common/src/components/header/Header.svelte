95575548ec3c16fb23e554721873ad76f7875ba9
amitpawar <amitpawar7475@gmail.com>
2025-05-16 15:46:39 +0530

    fix: add space in profile icon and right boundary [SPRW-986]

--- packages/@sparrow-common/src/components/header/Header.svelte
+++ packages/@sparrow-common/src/components/header/Header.svelte
@@ -382,7 +382,11 @@
           headerHeight={"28px"}
         >
           <div slot="pre-select" class="mb-1">
-            <div class="workspacename text-ds-font-size-12 text-ds-font-weight-regular text-ds-line-height-150">{currentTeamName}</div>
+            <div
+              class="workspacename text-ds-font-size-12 text-ds-font-weight-regular text-ds-line-height-150"
+            >
+              {currentTeamName}
+            </div>
           </div>
           <div
             slot="post-select"
@@ -390,7 +394,10 @@
             style="justify-content: center; align-items:center;"
           >
             <div class="lower-underline"></div>
-            <div class="view-all-workspace text-ds-font-size-12 text-ds-font-weight-medium" on:click={handleViewWorkspaces}>
+            <div
+              class="view-all-workspace text-ds-font-size-12 text-ds-font-weight-medium"
+              on:click={handleViewWorkspaces}
+            >
               <span>View all Workspaces</span>
               <Button
                 type="teritiary-regular"
@@ -399,7 +406,10 @@
               />
             </div>
             <div class="lower-underline"></div>
-            <div class="create-new-workspace text-ds-font-size-12 text-ds-font-weight-medium" on:click={onCreateWorkspace}>
+            <div
+              class="create-new-workspace text-ds-font-size-12 text-ds-font-weight-medium"
+              on:click={onCreateWorkspace}
+            >
               <span>Create New Workspace</span>
               <div style="align-content: flex-end;">
                 <Button
@@ -417,7 +427,9 @@
 
   <div
     class="d-flex align-items-center no-drag"
-    style="position: relative; display:flex; gap: 16px;"
+    style="position: relative; display:flex; gap: 16px; margin-right: {isWebApp
+      ? '16px'
+      : '0px'}"
   >
     {#if isGuestUser && isLoginBannerActive === false}
       <Tooltip
@@ -600,7 +612,7 @@
     {/if}
 
     {#if !isGuestUser}
-      <div class={"pe-1"}>
+      <div>
         <UserProfileModal
           {isGuestUser}
           item={sidebarModalItem}
