283aacd51f57145d94db6feb2f1525ae9b064c7c
kishangupta4514 <kishangupta4514@gmail.com>
2025-05-28 09:11:01 +0530

    add the workspace details page [SPRW-1085]

--- packages/@sparrow-common/src/icons/index.ts
+++ packages/@sparrow-common/src/icons/index.ts
@@ -3,3 +3,4 @@ export { default as SparrowPrimaryIcon } from "./SparrowPrimaryIcon.svelte";
 export { default as SparrowSecondaryIcon } from "./SparrowSecondaryIcon.svelte";
 export { default as SparkleFilled } from "./SparkleFilled.svelte";
 export { default as SparrowLogo } from "./SparrowLogo.svelte";
+export { default as SparrowOutlineIcon} from "./SparrowOutlineIcon.svelte"
