05d37549f6b0b60a8c04514790184c9d3cff6d8e
anish-techdome <anish.kumar@techdome.net.in>
2025-06-11 16:08:48 +0530

    fixed: version

--- apps/@sparrow-desktop/src/models/tab.model.ts
+++ apps/@sparrow-desktop/src/models/tab.model.ts
@@ -236,7 +236,7 @@ export const tabSchemaLiteral = {
   title: "Opened tabs that will be shown on dashboard",
   primaryKey: "tabId",
   type: "object",
-  version: 27,
+  version: 28,
   properties: {
     tabId: {
       // ---- RxDocumentId
