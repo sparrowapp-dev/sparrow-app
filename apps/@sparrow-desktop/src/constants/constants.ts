f128a51c93c6366063cdc6abe8268e69650c7edc
itsashishrath <anandashish654@gmail.com>
2025-06-13 15:59:38 +0530

    fix: removed the linitng error from constant

--- apps/@sparrow-desktop/src/constants/constants.ts
+++ apps/@sparrow-desktop/src/constants/constants.ts
@@ -26,16 +26,14 @@ const constants = {
     .VITE_AZURE_INSIGHTS_CONNECTION_STRING,
   BASE_URL: import.meta.env.VITE_BASE_URL,
   INTRO_DOCS_URL: import.meta.env.VITE_SPARROW_DOCS + "/docs/intro",
-  TESTFLOW_DOCS_URL:
-    import.meta.env.VITE_SPARROW_DOCS + "/docs/usermanual/testflows",
+  TESTFLOW_DOCS_URL: import.meta.env.VITE_SPARROW_DOCS + "/docs/usermanual/testflows",
   APP_ENVIRONMENT_PATH: import.meta.env.VITE_APP_ENVIRONMENT_PATH,
   SPARROW_AI_WEBSOCKET_URL: import.meta.env.VITE_SPARROW_AI_WEBSOCKET,
   SENTRY_DSN: import.meta.env.VITE_SENTRY_DSN,
   APP_ENVIRONMENT: import.meta.env.VITE_APP_ENVIRONMENT,
   SPARROW_WEB_APP_URL: import.meta.env.VITE_SPARROW_WEB_APP_URL,
   MARKETING_URL: import.meta.env.VITE_MARKETING_URL,
-  LINUX_INSTALL_DOCS:
-    import.meta.env.VITE_SPARROW_DOCS + "/docs/usermanual/linux-install",
+  LINUX_INSTALL_DOCS: import.meta.env.VITE_SPARROW_DOCS + "/docs/usermanual/linux-install",
 };
 
 export default constants;
