067d8c30999685c8fedc1d3c76a7d8dff64b9d6e
Md Asif Raza <itsmdasifraza@gmail.com>
2025-06-30 00:14:44 -0700

    feat: revert request tab animation

--- apps/@sparrow-web/src/constants/animation.ts
+++ apps/@sparrow-web/src/constants/animation.ts
@@ -14,26 +14,12 @@ export const pagesMotion = {
       duration: 0.28, // 280ms
     },
   },
-  ease: [0.42, 0, 0.58, 1],
   style: { zIndex: 10 },
 };
 
 export const scaleMotionProps = {
-  initial: { opacity: 0.2 },
-  animate: {
-    opacity: 1,
-    transition: {
-      ease: "easeInOut",
-      duration: 0.28, // 280ms
-    },
-  },
-  exit: {
-    opacity: 0,
-    transition: {
-      ease: "easeInOut",
-      duration: 0.28, // 280ms
-    },
-  },
-  ease: [0.42, 0, 0.58, 1],
-  style: { zIndex: 10 },
+    initial: { opacity: 0.2 },
+    animate: { opacity: 1, transition: { duration: 0.5 } },
+    exit: { opacity: 0 },
+    style: { zIndex: 10 },
 };
