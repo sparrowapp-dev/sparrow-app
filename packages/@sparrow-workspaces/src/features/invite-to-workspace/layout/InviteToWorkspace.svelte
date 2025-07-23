491b7cf0ba842f359a67551a2f5e9daa5a9f515a
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-07-23 16:27:38 +0530

    fix: updated the message as per figma mentioned.

--- packages/@sparrow-workspaces/src/features/invite-to-workspace/layout/InviteToWorkspace.svelte
+++ packages/@sparrow-workspaces/src/features/invite-to-workspace/layout/InviteToWorkspace.svelte
@@ -212,11 +212,10 @@
   {/if}
 </div>
 <div class="text-secondary-200 mt-2 sparrow-fs-12">
-  Note: You can invite only hub members to this workspace. Invited users will
-  have access only to <span style="color:white"
+  You can invite hub members or external collaborators to this workspace.
+  Invited people will have access to only the <span style="color:white"
     >{currentWorkspaceDetails.name}</span
-  >
-  workspace.
+  > workspace.
 </div>
 {#if plan?.name !== "Community"}
   <p class="text-fs-12 mt-3" style="color: var(--text-ds-neutral-400)">
