2d8ca8d9bdb4f7412cd1bb6b05b1ea36b127cada
Kishan Gupta <kishan.gupta@techdome.net.in>
2025-08-22 18:51:46 +0530

    fix the validation for the hub and workspace box

--- packages/@sparrow-common/src/features/create-team/constants/name.ts
+++ packages/@sparrow-common/src/features/create-team/constants/name.ts
@@ -1,6 +1,6 @@
 export const NAME_CONFIG = {
   TITLE: `Hub Name`,
-  MAX_TEXT_SIZE: 500,
+  MAX_TEXT_SIZE: 100,
   REQUIRED_ERROR_MESSAGE: `Please enter your hub name.`,
   PLACEHOLDER: `Enter your hub name`,
   INVALID_ERROR_MESSAGE: `Hub name cannot contain only special characters.`,
