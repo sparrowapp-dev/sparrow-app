c59b048aba0d815bea9db4f48857ade91dc6de70
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-09-08 15:20:55 +0530

    fix: generate mock data sparrow edge issue

--- apps/@sparrow-desktop/src/pages/workspace-page/sub-pages/RestExplorer/RestExplorerPage.ViewModel.ts
+++ apps/@sparrow-desktop/src/pages/workspace-page/sub-pages/RestExplorer/RestExplorerPage.ViewModel.ts
@@ -4273,7 +4273,6 @@ class RestExplorerViewModel {
       progressiveTab.property.request.body.raw = response;
       this.tab = progressiveTab;
       progressiveTab.isSaved = false;
-      progressiveTab.isSaved = false;
       progressiveTab.persistence = TabPersistenceTypeEnum.PERMANENT;
       await this.tabRepository.updateTab(progressiveTab.tabId, progressiveTab);
       this.compareRequestWithServer();
