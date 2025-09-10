cca6b35c1ed5f9273c226566138258fd47870c4c
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-09-10 20:36:20 +0530

    fix: mock data condition based on role

--- packages/@sparrow-workspaces/src/features/rest-explorer/components/request-navigator/RequestNavigator.svelte
+++ packages/@sparrow-workspaces/src/features/rest-explorer/components/request-navigator/RequestNavigator.svelte
@@ -15,7 +15,7 @@
   export let isGenerateMockDataModal: boolean = false;
   import { ThreeDotIcon } from "@sparrow/library/assets";
   import { SparkleColoredIcon } from "@sparrow/common/icons";
-  import { TeamRole } from "@sparrow/common/enums";
+  import { TeamRole, WorkspaceRole } from "@sparrow/common/enums";
 
   let tabs: {
     name: string;
@@ -114,7 +114,7 @@
 </script>
 
 <div style="padding-bottom: 12px; position:relative;">
-  {#if (requestStateSection === RequestSectionEnum.HEADERS || requestStateSection === RequestSectionEnum.PARAMETERS || requestStateSection === RequestSectionEnum.REQUEST_BODY) && !isGuestUser && (userRole === TeamRole.TEAM_ADMIN || userRole === TeamRole.TEAM_MEMBER || userRole === TeamRole.TEAM_OWNER)}
+  {#if (requestStateSection === RequestSectionEnum.HEADERS || requestStateSection === RequestSectionEnum.PARAMETERS || requestStateSection === RequestSectionEnum.REQUEST_BODY) && !isGuestUser && (userRole === TeamRole.TEAM_ADMIN || userRole === WorkspaceRole.WORKSPACE_EDITOR || userRole === TeamRole.TEAM_OWNER)}
     <div
       class="button-container"
       style="position: absolute; top: 6px; right: 10px;"
