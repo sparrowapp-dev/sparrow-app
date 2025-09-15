73baf18a7c9eadbe10ad30ae5f8e905dcf1be108
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-09-15 13:59:53 +0530

    feat: updated tour guide name to use it command one

--- packages/@sparrow-workspaces/src/features/request-tab-tour-guide/utils/requestTabCardfunctions.ts
+++ packages/@sparrow-workspaces/src/features/request-tab-tour-guide/utils/requestTabCardfunctions.ts
@@ -1,22 +1,52 @@
 import { get } from "svelte/store";
-import { requestTabTestDemo, requestTabTestNoCodeStep } from "../../../stores";
+import {
+  requestTabTestDemo,
+  requestTabTestNoCodeStep,
+  requestTabTestScriptDemo,
+  requestTabTestScriptStep,
+} from "../../../stores";
 
 const MAX_STEP = 5;
 
-const resetTour = () => {
+const resetNoCodeTour = () => {
   requestTabTestNoCodeStep.set(0);
   requestTabTestDemo.set(false);
 };
 
+const resetScriptTour = () => {
+  requestTabTestScriptStep.set(0);
+  requestTabTestScriptDemo.set(false);
+};
+
 export const handleNextStep = () => {
-  const currentStep = get(requestTabTestNoCodeStep);
-  if (currentStep < MAX_STEP) {
-    requestTabTestNoCodeStep.set(currentStep + 1);
-  } else {
-    resetTour();
+  // Check if No Code tour is active
+  if (get(requestTabTestDemo)) {
+    const currentStep = get(requestTabTestNoCodeStep);
+    if (currentStep < MAX_STEP) {
+      requestTabTestNoCodeStep.set(currentStep + 1);
+    } else {
+      resetNoCodeTour();
+    }
+    return;
+  }
+
+  // Check if Script tour is active
+  if (get(requestTabTestScriptDemo)) {
+    const currentStep = get(requestTabTestScriptStep);
+    if (currentStep < MAX_STEP) {
+      requestTabTestScriptStep.set(currentStep + 1);
+    } else {
+      resetScriptTour();
+    }
+    return;
   }
 };
 
 export const handleCloseTour = () => {
-  resetTour();
+  if (get(requestTabTestDemo)) {
+    resetNoCodeTour();
+  }
+  if (get(requestTabTestScriptDemo)) {
+    resetScriptTour();
+  }
 };
