29f7e5abd992e2b92acee8be56329dec7abf837d
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-09-17 14:01:18 +0530

    fix: javascript syntax error support

--- packages/@sparrow-workspaces/src/features/rest-explorer/components/request-tests/sub-components/script/Script.svelte
+++ packages/@sparrow-workspaces/src/features/rest-explorer/components/request-tests/sub-components/script/Script.svelte
@@ -26,7 +26,7 @@
   export let isTestCasesGenerating;
 
   type SplitDirection = "vertical" | "horizontal";
-  type EditorLanguage = "JavaScript";
+  type EditorLanguage = "TestJavaScript";
 
   // Snippet type (based on your utils/common-snippets.ts)
   interface Snippet {
@@ -35,7 +35,7 @@
   }
 
   export let tabSplitDirection: SplitDirection = "vertical";
-  export let lang: EditorLanguage = "JavaScript";
+  export let lang: EditorLanguage = "TestJavaScript";
 
   export let isBodyBeautified: boolean = false;
 
