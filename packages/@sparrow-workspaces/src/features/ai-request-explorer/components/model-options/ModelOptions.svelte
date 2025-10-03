a1891382f050e6eb143f1a94a6efc47eb4555889
prayas17 <prayas1711@gmail.com>
2025-10-03 11:28:55 +0530

    fix: Gemini LLM Issue

--- packages/@sparrow-workspaces/src/features/ai-request-explorer/components/model-options/ModelOptions.svelte
+++ packages/@sparrow-workspaces/src/features/ai-request-explorer/components/model-options/ModelOptions.svelte
@@ -77,9 +77,9 @@
       disabled: false,
       icon: GoogleVectorIcon,
       models: [
-        { name: "Gemini 1.5 Flash", id: "gemini-1.5-flash" },
-        { name: "Gemini 1.5 Flash 8B", id: "gemini-1.5-flash-8b" },
-        { name: "Gemini 1.5 Pro", id: "gemini-1.5-pro" },
+        // { name: "Gemini 1.5 Flash", id: "gemini-1.5-flash" },
+        // { name: "Gemini 1.5 Flash 8B", id: "gemini-1.5-flash-8b" },
+        // { name: "Gemini 1.5 Pro", id: "gemini-1.5-pro" },
         { name: "Gemini 2.0 Flash", id: "gemini-2.0-flash" },
       ],
     },
