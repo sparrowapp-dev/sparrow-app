d7ad3af5217751789b78664d727369651229d592
Aakash Reddy <aakash.reddy@techdome.net.in>
2025-10-13 18:12:25 +0530

    fix: added type condition as well

--- apps/@sparrow-web/src/pages/workspace-page/sub-pages/RestExplorer/RestExplorerPage.ViewModel.ts
+++ apps/@sparrow-web/src/pages/workspace-page/sub-pages/RestExplorer/RestExplorerPage.ViewModel.ts
@@ -90,7 +90,7 @@ import type { Socket } from "socket.io-client";
 import { restExplorerDataStore } from "@sparrow/workspaces/features/rest-explorer/store";
 import { CollectionTabAdapter } from "@app/adapter";
 import type { Tab } from "@sparrow/common/types/workspace/tab";
-import { TabPersistenceTypeEnum } from "@sparrow/common/types/workspace/tab";
+import { TabPersistenceTypeEnum, TabTypeEnum } from "@sparrow/common/types/workspace/tab";
 import {
   CollectionAuthTypeBaseEnum,
   CollectionItemTypeBaseEnum,
@@ -1663,7 +1663,7 @@ class RestExplorerViewModel {
     const allTabs = await this.tabRepository.getTabLs();
     if (!allTabs || allTabs.length === 0) return;
     for (const tab of allTabs) {
-      if (tab.id !== currentTab.id) {
+      if (tab.id !== currentTab.id && tab.type === TabTypeEnum.REQUEST) {
         let progressiveTab = createDeepCopy(tab);
         const conversationLength =
           progressiveTab.property?.request?.ai?.conversations?.length ?? 0;
