e911913b8c7944f3c831265325822894d6b82de4
Md Asif Raza <itsmdasifraza@gmail.com>
2025-10-16 17:22:48 +0530

    SPRW-2373 [Pre-script] Script Execution and Error Reporting

--- packages/@sparrow-workspaces/src/features/rest-explorer/components/request-tests/sub-components/pre-script/PreScript.svelte
+++ packages/@sparrow-workspaces/src/features/rest-explorer/components/request-tests/sub-components/pre-script/PreScript.svelte
@@ -553,7 +553,7 @@
           on:change={handleCodeMirrorChange}
           isEditable={true}
           autofocus={true}
-          placeholder={`// What are the Pre-Request tests?\n// It runs before your request is sent. Use them to set variables, headers, prepare data.\n// For example: Need a timestamp for every request?\n// sp.environment.set("timestamp", Date.now());\n//     sp.request.headers.add({ key: "Auth-Token", value: "12345" });\n// });\n\n// You can:\n// - Use "Snippets" to insert common Pre-Requests\n// - Type a prompt and click "Generate" to create a Pre-Requests using AI\n// - Or, write Pre-Requests manually using scripting`}
+          placeholder={`// What are the Pre-Request tests?\n// It runs before your request is sent. Use them to set variables, headers, prepare data.\n// For example: Need a timestamp for every request?\n// sp.environment.set("timestamp", Date.now());\n//     sp.request.headers.set("Auth-Token", "12345");\n// });\n\n// You can:\n// - Use "Snippets" to insert common Pre-Requests\n// - Type a prompt and click "Generate" to create a Pre-Requests using AI\n// - Or, write Pre-Requests manually using scripting`}
           {isBodyBeautified}
           beautifySyntaxCallback={updateBeautifiedState}
         />
