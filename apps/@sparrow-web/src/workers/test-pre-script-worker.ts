105914ac0f43281d45558e1c60c7a8fe2145fae0
Md Asif Raza <itsmdasifraza@gmail.com>
2025-10-16 18:24:12 +0530

    feat: map workflow to 2.34.0

--- apps/@sparrow-web/src/workers/test-pre-script-worker.ts
+++ apps/@sparrow-web/src/workers/test-pre-script-worker.ts
@@ -121,55 +121,7 @@ import { XMLParser } from "fast-xml-parser";
 
 self.onmessage = (e) => {
   const { javaScriptTestCases, request, env, auth } = e.data;
-debugger;
-// const headersObject = Object.fromEntries(
-//                   JSON.parse(decodeData[2]).map(({ key, value }) => [
-//                     key,
-//                     value,
-//                   ]),
-//                 );
-//            let reqBody;
-//                 if (decodeData[4] === "application/json") {
-//                   // tried to handle js but that is treated as text/plain, skipping that for now
-//                   try {
-//                     reqBody = JSON.stringify(JSON.parse(decodeData[3]));
-//                   } catch (e) {
-//                     reqBody = JSON.stringify({});
-//                   }
-//                 } else if (
-//                   decodeData[4] === "multipart/form-data" ||
-//                   decodeData[4] === "application/x-www-form-urlencoded"
-//                 ) {
-//                   const formDataObject = Object.fromEntries(
-//                     JSON.parse(decodeData[3]).map(({ key, value }) => [
-//                       key,
-//                       value,
-//                     ]),
-//                   );
-//                   reqBody = JSON.stringify(formDataObject || {});
-//                 } else {
-//                   reqBody = decodeData[3];
-//                 }
-
-//                 const reqParam = {};
-//                 const params = new URL(decodeData[0]).searchParams;
-
-//                 for (const [key, value] of params.entries()) {
-//                   reqParam[key] = value;
-//                 }
-
-
-//           const preRequest =  {
-//                     headers: JSON.stringify(headersObject || {}),
-//                     body: reqBody,
-//                     parameters: JSON.stringify(reqParam || {}),
-//                     url: decodeData[0],
-
-//                   };
-
-
-
-  const tests = [];
+  const tests: any[] = [];
 
   const sp = {
     request: {
@@ -189,6 +141,9 @@ debugger;
             return {};
           }
         },
+        set: (data: string) => {
+          request.body.raw = data  || "";
+        },
        },
        formdata:{
         text: () => {
@@ -196,6 +151,51 @@ debugger;
         },
         json: () => {
           return request.body.formdata;
+        },
+        set: (key: any, value: any) => {
+          if (!request.body.formdata) request.body.formdata = [];
+          const existingIndex = request.body.formdata.findIndex((item: any) => item.key === key);
+          if (existingIndex !== -1) {
+            request.body.formdata[existingIndex].value = value;
+          } else {
+            request.body.formdata.push({ key, value, checked: true });
+          }
+        },
+        get: (key: any) => {
+          if (!request.body.formdata) return "";
+          const item = request.body.formdata.find((item: any) => item.key === key);
+          return item ? item.value : "";
+        },
+        remove: (key: any) => {
+          if (request.body.formdata) {
+            request.body.formdata = request.body.formdata.filter((item: any) => item.key !== key);
+          }
+        },
+        clear: () => {
+          request.body.formdata = [];
+        },
+        has: (key: any) => {
+          if (!request.body.formdata) return false;
+          return request.body.formdata.some((item: any) => item.key === key);
+        },
+        enable: (key: any) => {
+          if (request.body.formdata) {
+            const item = request.body.formdata.find((item: any) => item.key === key);
+            if (item) item.checked = true;
+          }
+        },
+        disable: (key: any) => {
+          if (request.body.formdata) {
+            const item = request.body.formdata.find((item: any) => item.key === key);
+            if (item) item.checked = false;
+          }
+        },
+        isChecked: (key: any) => {
+          if (request.body.formdata) {
+            const item = request.body.formdata.find((item: any) => item.key === key);
+            if (item) return item.checked;
+          }
+          return false;
         },
        },
        urlencoded: {
@@ -205,11 +205,75 @@ debugger;
         json: () => {
           return request.body.urlencoded;
         },
-       },
+        set: (key: any, value: any) => {
+          if (!request.body.urlencoded) request.body.urlencoded = [];
+          const existingIndex = request.body.urlencoded.findIndex((item: any) => item.key === key);
+          if (existingIndex !== -1) {
+            request.body.urlencoded[existingIndex].value = value;
+          } else {
+            request.body.urlencoded.push({ key, value, checked: true });
+          }
+        },
+        get: (key: any) => {
+          if (!request.body.urlencoded) return "";
+          const item = request.body.urlencoded.find((item: any) => item.key === key);
+          return item ? item.value : "";
+        },
+        remove: (key: any) => {
+          if (request.body.urlencoded) {
+            request.body.urlencoded = request.body.urlencoded.filter((item: any) => item.key !== key);
+          }
+        },
+        clear: () => {
+          request.body.urlencoded = [];
+        },
+        has: (key: any) => {
+          if (!request.body.urlencoded) return false;
+          return request.body.urlencoded.some((item: any) => item.key === key);
+        },
+        enable: (key: any) => {
+          if (request.body.urlencoded) {
+            const item = request.body.urlencoded.find((item: any) => item.key === key);
+            if (item) item.checked = true;
+          }
+        },
+        disable: (key: any) => {
+          if (request.body.urlencoded) {
+            const item = request.body.urlencoded.find((item: any) => item.key === key);
+            if (item) item.checked = false;
+          }
+        },
+        isChecked: (key: any) => {
+          if (request.body.urlencoded) {
+            const item = request.body.urlencoded.find((item: any) => item.key === key);
+            if (item) return item.checked;
+          }
+          return false;
+        }
+       }
       },
       url: {
         text: () => {
           return request.url;
+        },
+        set: (newUrl: any) => {
+          request.url = newUrl;
+        },
+        getBaseUrl: () => {
+          try {
+            const url = new URL(request.url);
+            return `${url.protocol}//${url.host}`;
+          } catch {
+            return "";
+          }
+        },
+        getPath: () => {
+          try {
+            const url = new URL(request.url);
+            return url.pathname;
+          } catch {
+            return "";
+          }
         },
       },
       headers: {
@@ -219,6 +283,49 @@ debugger;
         json: () => {
           return request.headers;
         },
+        set: (key: any, value: any) => {
+          if (!request.headers) request.headers = [];
+          const existingIndex = request.headers.findIndex((item: any) => item.key === key);
+          if (existingIndex !== -1) {
+            request.headers[existingIndex].value = value;
+          } else {
+            request.headers.push({ key, value, checked: true });
+          }
+        },
+        get: (key: any) => {
+          if (!request.headers) return "";
+          const item = request.headers.find((item: any) => item.key === key);
+          return item ? item.value : "";
+        },
+        remove: (key: any) => {
+          if (request.headers) {
+            request.headers = request.headers.filter((item: any) => item.key !== key);
+          }
+        },
+        clear: () => {
+          request.headers = [];
+        },
+        has: (key: any) => {
+          if (!request.headers) return false;
+          return request.headers.some((item: any) => item.key === key);
+        },
+        enable: (key: any) => {
+          if (request.headers) {
+            const item = request.headers.find((item: any) => item.key === key);
+            if (item) item.checked = true;
+          }
+        },
+        disable: (key: any) => {
+          if (request.headers) {
+            const item = request.headers.find((item: any) => item.key === key);
+            if (item) item.checked = false;
+          }
+        },
+        isChecked: (key: any) => {
+          if (!request.headers) return false;
+          const item = request.headers.find((item: any) => item.key === key);
+          return item ? item.checked : false;
+        },
       },
       parameters: {
         text: () => {
@@ -227,74 +334,165 @@ debugger;
         json: () => {
           return request.queryParams;
         },
+        set: (key: any, value: any) => {
+          if (!request.queryParams) request.queryParams = [];
+          const existingIndex = request.queryParams.findIndex((item: any) => item.key === key);
+          if (existingIndex !== -1) {
+            request.queryParams[existingIndex].value = value;
+          } else {
+            request.queryParams.push({ key, value, checked: true });
+          }
+        },
+        get: (key: any) => {
+          if (!request.queryParams) return "";
+          const item = request.queryParams.find((item: any) => item.key === key);
+          return item ? item.value : "";
+        },
+        remove: (key: any) => {
+          if (request.queryParams) {
+            request.queryParams = request.queryParams.filter((item: any) => item.key !== key);
+          }
+        },
+        clear: () => {
+          request.queryParams = [];
+        },
+        has: (key: any) => {
+          if (!request.queryParams) return false;
+          return request.queryParams.some((item: any) => item.key === key);
+        },
+        enable: (key: any) => {
+          if (request.queryParams) {
+            const item = request.queryParams.find((item: any) => item.key === key);
+            if (item) item.checked = true;
+          }
+        },
+        disable: (key: any) => {
+          if (request.queryParams) {
+            const item = request.queryParams.find((item: any) => item.key === key);
+            if (item) item.checked = false;
+          }
+        },
+        isChecked: (key: any) => {
+          if (!request.queryParams) return false;
+          const item = request.queryParams.find((item: any) => item.key === key);
+          return item ? item.checked : false;
+        },
+
       },
 
       method: {
         text: () => {
           return request.method;
         },
+        set: (newMethod: any) => {
+          const validMethods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+          if (validMethods.includes(newMethod.toUpperCase())) {
+            request.method = newMethod.toUpperCase();
+          } else {
+            throw new Error(`Invalid HTTP method: ${newMethod}`);
+          }
+        },
       },
       auth:{
         bearerToken : {
           text: () => {
-            return request.auth.bearerToken;
+            return request.auth?.bearerToken || "";
+          },
+          set: (token: any) => {
+            request.auth.bearerToken = token || "";
+          },
+          clear: () => {
+            request.auth.bearerToken = "";
           },
         },
         basicAuth:{
           username:{
             text: () => {
-              return request.auth.basicAuth.username;
+              return request.auth?.basicAuth?.username || "";
+            },
+            set: (username: any) => {
+              request.auth.basicAuth.username = username || "";
             },
           },
           password:{
             text: () => {
-              return request.auth.basicAuth.password;
+              return request.auth?.basicAuth?.password || "";
             },
-          }
+            set: (password: any) => {
+              request.auth.basicAuth.password = password || "";
+            },
+          },
+          set: (username: any, password: any) => {
+            request.auth.basicAuth.username = username || "";
+            request.auth.basicAuth.password = password || "";
+          },
+          clear: () => {
+              request.auth.basicAuth.password = "";
+              request.auth.basicAuth.username = "";
+          },
         },
         apiKey:{
           key:{
             text: () => {
-              return request.auth.apiKey.authKey;
-            }
+              return request.auth?.apiKey?.authKey || "";
+            },
+            set: (key: any) => {
+              request.auth.apiKey.authKey = key || "";
+            },
           },
           value:{
             text: () => {
-              return request.auth.apiKey.authValue;
-            }
-          }
+              return request.auth?.apiKey?.authValue || "";
+            },
+            set: (value: any) => {
+              request.auth.apiKey.authValue = value || "";
+            },
+          },
+          set: (key: any, value: any) => {
+            request.auth.apiKey.authKey = key || "";
+            request.auth.apiKey.authValue = value || "";
+          },
+          clear: () => {
+            request.auth.apiKey.authKey = "";
+            request.auth.apiKey.authValue =  "";
+          },
+        },
+        clear: () => {
+          request.auth.apiKey.authKey = "";
+          request.auth.apiKey.authValue =  "";
+          request.auth.bearerToken = "";
+          request.auth.basicAuth.password = "";
+          request.auth.basicAuth.username = "";
         }
       }
-
-  
     },
-    test: (name, fn) => {
+    test: (name: any, fn: any) => {
       try {
         fn();
         tests.push({ name, passed: true });
-      } catch (err) {
+      } catch (err: any) {
         tests.push({ name, passed: false, error: err.message });
       }
     },
     environment:{
-      set:(key, value)=>{
+      set:(key: any, value: any)=>{
         if(key){
           for(const e of env){
-            if(e.key === key){
+            if(e.key === key && e.type === "E"){
               e.value = value || "";
               break;
             }
           }
           // if didnt found the key then add new
-          if(!env.find((e)=>e.key === key)){
-            env.push({key, value: value || ""});
+          if(!env.find((e: any)=>e.key === key && e.type === "E")){
+            env.unshift({key, value: value || "", type: "E"});
           }
         }
       },
-      get:(key)=>{
+      get:(key: any)=>{
         if(key){
           for(const e of env){
-            if(e.key === key){
+            if(e.key === key && e.type === "E"){
               return e.value || "";
             }
           }
@@ -305,24 +503,24 @@ debugger;
     },
 
     global:{
-      set:(key, value)=>{
+      set:(key: any, value: any)=>{
         if(key){
           for(const e of env){
-            if(e.key === key){
+            if(e.key === key && e.type === "G"){
               e.value = value || "";
               break;
             }
           }
           // if didnt found the key then add new
-          if(!env.find((e)=>e.key === key)){
-            env.push({key, value: value || ""});
+          if(!env.find((e: any)=>e.key === key  && e.type === "G")){
+            env.push({key, value: value || "", type: "G"});
           }
         }
       },
-      get:(key)=>{
+      get:(key: any)=>{
         if(key){
           for(const e of env){
-            if(e.key === key){
+            if(e.key === key  && e.type === "G"){
               return e.value || "";
             }
           }
@@ -331,18 +529,25 @@ debugger;
         return "";
       }
     },
-    xmlToJSON: (xml) => {
+    uuid: () => {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    },
+    xmlToJSON: (xml: any) => {
       const parser = new XMLParser();
       return parser.parse(xml);
     },
-    expect,
+    expect
   };
 
   try {
     const fn = new Function("sp", javaScriptTestCases);
     fn(sp);
     self.postMessage({ success: true, tests, request, env, auth });
-  } catch (err) {
+  } catch (err: any) {
     self.postMessage({ success: false, error: `${err.name} - ${err.message}`, tests, request, env, auth });
   }
 };
