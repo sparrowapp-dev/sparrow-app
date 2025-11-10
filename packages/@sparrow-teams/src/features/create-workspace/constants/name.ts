da48b5976007a14ffe1de7ea2bd1cef206a69a43
Harshit Soni <harshit.soni@techdome.net.in>
2025-11-10 18:46:56 +0530

    Validation for Hub Names SPRW-691

--- packages/@sparrow-teams/src/features/create-workspace/constants/name.ts
+++ packages/@sparrow-teams/src/features/create-workspace/constants/name.ts
@@ -2,4 +2,5 @@ export const NAME_CONFIG = {
   TITLE: `Workspace Name`,
   REQUIRED_ERROR_MESSAGE: `Workspace name cannot be empty.`,
   PLACEHOLDER: `Enter workspace name`,
+  INVALID_ERROR_MESSAGE: `Workspace names can contain combination of letters, digits and these special characters (.,-,_). Please provide the workspace names accordingly.`,
 };
